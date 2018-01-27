fd2a99a2306837091d822cd47917b717e2ba5c7b
Main <steeman.rene1@gmail.com>
2018-01-27 19:54:54 +0100

    cleenup needed

---
+++ html/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="../css/header.min.css">
+
+    <title>Informatica</title>
+  </head>
+  <body>
+
+	<!-- navbar -->
+	<nav class="navbar navbar-expand-md top-navbar">
+  	    	<div class="navbar-nav navbar-nav-top">
+  	    		<div class="nav-item top-nav" href="#">Informatica</div>
+  			</div>
+	</nav>
+
+	<nav class="navbar navbar-expand-md">
+		<div class="navbar-nav mr-auto">
+			<a class="nav-item" href="index.html">startpagina</a>
+	    </div>
+
+		<div class="navbar-nav mr-auto">
+		</div>
+
+        <div class="navbar-nav ml-auto">
+           <a class="nav-item" href="account.html">Hello Debugger</a>
+	   </div>
+
+	</nav>
+
+    

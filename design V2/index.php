5e6387c1f0d5583634f73b89afe16963887ff06a
Main <steeman.rene1@gmail.com>
2018-02-18 17:11:25 +0100

    design V2 start

---
+++ design V2/index.php
@@ -0,0 +1,59 @@
+<head>
+	<link rel="stylesheet" href="./css/main.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+    <title>Informatica</title>
+<head>
+
+<header>
+	<div class="navigation">
+		<h1>
+			INFORCA
+		</h1>
+
+		<div class="nav-bar">
+			<div>HOME</div>
+			<div>OVERVIEW</div>
+			<div>PLANNER</div>
+		</div>
+
+		<div class="welcome">
+			<div>
+				Hi User sdhfgasdggdasgasdsadfhjgdsaerhhadf
+			</div>
+			<div>
+				logout
+			</div>
+		</div>
+	</div>
+
+</header>
+
+<body>
+
+	<div class="title">
+		<h2>
+			Theorie overzicht
+		</h2>
+	</div>
+
+	<div class="bar">
+		<h3>
+			Kern hoofdstukken
+		</h3>
+	</div>
+
+	<div class="chapter-tiles">
+		<div class="tile">H1</div>
+		<div class="tile">H2</div>
+		<div class="tile">H3</div>
+		<div class="tile">H4</div>
+		<div class="tile">H5</div>
+		<div class="tile">H6</div>
+		<div class="tile">H7</div>
+		<div class="tile">H8</div>
+	</div>
+
+
+</body>

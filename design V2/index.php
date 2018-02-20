71d08838cad318e8a35a4ad2cdcf7b922f25e5e9
Main <steeman.rene1@gmail.com>
2018-02-20 08:43:30 +0100

    redesign part 9

--- design V2/index.php
+++ design V2/index.php
@@ -177,5 +177,18 @@
 
 	</div>
 
+	<footer>
+		<div class="bar-large">
+			<div class="contact">
+				Neem gerust contact op voor vragen, suggesties, adviezen en overigen. U mag binnen 24 een antwoord verwachten via de mail.
+			</div>
+			<div class="contact-info">
+				<a id="phone" href="tel:+045622155216">06-22155216</a>
+				<a id="mail" href="mailto:koffieandcode@gmail.com?SUBJECT=Inforca">koffieandcode@gmail.com</a>
+			</div>
+		</div>
+	</footer>
+
+
 
 </body>

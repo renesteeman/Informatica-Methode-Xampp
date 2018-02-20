999788731d0b9e8a8ecf95f6b001f4c49af2252a
Main <steeman.rene1@gmail.com>
2018-02-20 21:29:06 +0100

    V11

--- design V2/scripts/UI.js
+++ design V2/scripts/UI.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+	//burger icon
 	$(".burger-icon").click(function(){
 		$(".nav-burger").toggleClass("active");
 	});
 
+	//chapter tiles
 	$(".tile").click(function(){
-		$(".tile").not(this).toggle();
+		$(this).siblings().toggle();
 		$(this).toggleClass("active");
 
 	});

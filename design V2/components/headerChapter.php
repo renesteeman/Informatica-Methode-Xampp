878985b5b3e2ab5f59c1152ffdbad3562cd141aa
Main <steeman.rene1@gmail.com>
2018-02-21 17:05:29 +0100

    new animation and more modularity

---
+++ design V2/components/headerChapter.php
@@ -0,0 +1,68 @@
+<head>
+	<!-- external includes -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- internal includes -->
+	<link rel="stylesheet" href="./css/main.min.css">
+	<script src="scripts/UI.js"></script>
+
+    <title>Informatica</title>
+<head>
+
+<header>
+	<div class="navigation">
+
+		<h1>
+			INFORCA
+		</h1>
+
+		<div class="nav-wide">
+
+			<div class="nav-bar">
+				<a href="#">HOME</a>
+				<a href="#">OVERVIEW</a>
+				<a href="#">PLANNER</a>
+			</div>
+
+			<div class="empty">
+				<div class="welcome">
+					<div>
+						Hi User sdhfgasdggdasgasdsadfhjgdsaerhhadf
+					</div>
+					<div>
+						logout
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nav-burger">
+			<div class="burger-icon">
+
+				<div class="bar1"></div>
+				<div class="bar2"></div>
+				<div class="bar3"></div>
+
+			</div>
+
+			<div class="burger-content">
+				<div class="nav-bar">
+					<a href="#">HOME</a>
+					<a href="#">OVERVIEW</a>
+					<a href="#">PLANNER</a>
+				</div>
+
+				<div class="welcome">
+					<div>
+						Hi User sdhfgasdggdasgasdsadfhjgdsaerhhadf
+					</div>
+					<div>
+						logout
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</header>

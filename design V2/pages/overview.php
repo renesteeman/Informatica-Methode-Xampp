876ca34e48b2d45bf0ef58f72a8c7f849c7b7e6c
Main <steeman.rene1@gmail.com>
2018-03-03 21:00:48 +0100

    chrome shadow bug work around added

--- design V2/pages/overview.php
+++ design V2/pages/overview.php
@@ -23,94 +23,102 @@
 		<!-- the table as a whole -->
 		<div class="table">
 
-		<?php
+			<?php
 
-			//if logged in show class
-			//TODO check if person is teacher
-			if (isset($_SESSION["username"])){
+				//if logged in show class
+				//TODO check if person is teacher
+				if (isset($_SESSION["username"])){
 
-				$user = $_SESSION["username"];
+					$user = $_SESSION["username"];
 
-				$klassen = [];
-				$klassen['klas'] = [];
+					$klassen = [];
+					$klassen['klas'] = [];
 
-				$sql = "SELECT school, klas FROM users WHERE username='$user'";
-
-				if (mysqli_query($conn, $sql)) {
-					//find school of teacher
-					$result = mysqli_query($conn, $sql);
-					$result = mysqli_fetch_assoc($result);
-					$school = $result['school'];
-
-					$sql = "SELECT naam, username, klas FROM `users` WHERE school='$school' AND functie='leerling'";
+					$sql = "SELECT school, klas FROM users WHERE username='$user'";
 
 					if (mysqli_query($conn, $sql)) {
-
+						//find school of teacher
 						$result = mysqli_query($conn, $sql);
+						$result = mysqli_fetch_assoc($result);
+						$school = $result['school'];
 
-						if (mysqli_num_rows($result) > 0) {
-						    // output data of each row of names with class
+						$sql = "SELECT naam, username, klas FROM `users` WHERE school='$school' AND functie='leerling'";
 
-						    while($row = mysqli_fetch_assoc($result)) {
-								$klas = $row["klas"];
+						if (mysqli_query($conn, $sql)) {
 
-								$naam = $row["naam"];
-								$username = $row["username"];
+							$result = mysqli_query($conn, $sql);
 
-								//add userinfo to right class
-								$userinfo = ['naam'=>$naam, 'username'=>$username];
+							if (mysqli_num_rows($result) > 0) {
+							    // output data of each row of names with class
 
-								$klassen['klas'][$klas][] = $userinfo;
+							    while($row = mysqli_fetch_assoc($result)) {
+									$klas = $row["klas"];
 
-						    }
+									$naam = $row["naam"];
+									$username = $row["username"];
+
+									//add userinfo to right class
+									$userinfo = ['naam'=>$naam, 'username'=>$username];
+
+									$klassen['klas'][$klas][] = $userinfo;
+
+							    }
+
+							} else {
+							    echo "0 results";
+							}
+
+							//How many classes are there?
+							$Nclasses = count($klassen['klas']);
+
+							//put the classes in the right order
+							ksort($klassen['klas']);
+
+							//Show me these (the nice way)
+							$AllClasses = array_keys($klassen['klas']);
+
+							for($i=0; $i < $Nclasses; $i++){
+								$CurrentClass = $AllClasses[$i];
+
+								$StudentsCurrentClass[] = $klassen['klas'][$CurrentClass];
+
+								$NStudents = array_map("count", $StudentsCurrentClass);
+								$NStudentsCurrentClass = ($NStudents[$i]);
+
+								echo'
+								<div class="class">
+									<!-- table header for this class-->
+									<div class="classHeader">
+										<span class="klas">'.$CurrentClass.'</span>
+										<span class="Nleerlingen">'.$NStudentsCurrentClass.' leerlingen </span>
+										<span class="icons">
+											<span class="Mail image"><img src="../icons/mail.svg"/></span>
+											<span class="Arrow image"><img src="../icons/arrow.svg" class="arrow"/></span>
+										</span>
+									</div>
+
+									<!-- table content for this class-->
+									<div class="classContent">';
+
+
+									for($j=0; $j<$NStudentsCurrentClass; $j++){
+										echo '
+
+												<div class="row">
+													<span class="name">'.$j.'</span>
+													<span class="progress">icon</span>
+												</div>
+
+										';
+
+									}
+
+							echo '</div></div>';
+
+							}
 
 						} else {
-						    echo "0 results";
-						}
-
-						//How many classes are there?
-						$Nclasses = count($klassen['klas']);
-
-						//put the classes in the right order
-						ksort($klassen['klas']);
-
-						//Show me these (the nice way)
-						$AllClasses = array_keys($klassen['klas']);
-
-						for($i=0; $i < $Nclasses; $i++){
-							$CurrentClass = $AllClasses[$i];
-
-							$StudentsCurrentClass[] = $klassen['klas'][$CurrentClass];
-
-							$NStudents = array_map("count", $StudentsCurrentClass);
-							$NStudentsCurrentClass = ($NStudents[$i]);
-
-							echo'
-							<div class="class">
-								<!-- table header for this class-->
-								<div class="classHeader">
-									<span class="klas">'.$CurrentClass.'</span>
-									<span class="Nleerlingen">'.$NStudentsCurrentClass.' leerlingen </span>
-									<span class="icons">
-										<span class="Mail image"><img src="../icons/mail.svg"/></span>
-										<span class="Arrow image"><img src="../icons/arrow.svg" class="arrow"/></span>
-									</span>
-								</div>
-
-								<!-- table content for this class-->';
-
-
-
-
-								echo '
-									<div class="classContent">
-										<div class="row">
-											<span class="name">Student</span>
-											<span class="progress">icon</span>
-										</div>
-									</div>
-								</div>
-								';
+							echo "Error with sql execution, please report to admin </br>";
 						}
 
 					} else {
@@ -118,14 +126,10 @@
 					}
 
 				} else {
-					echo "Error with sql execution, please report to admin </br>";
+					echo 'You are not a teacher or have no students';
 				}
 
-			} else {
-				echo 'You are not a teacher or have no students';
-			}
-
-		?>
+			?>
 
 		</div>
 	</div>

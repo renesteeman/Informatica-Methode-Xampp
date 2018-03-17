0df4e9a0509509f4c566427e4ad69004c8d1a69e
Main <steeman.rene1@gmail.com>
2018-03-17 21:41:50 +0100

    Starting with displaying groups

--- design V2/pages/overview.php
+++ design V2/pages/overview.php
@@ -152,110 +152,119 @@
 
 			<?php
 
-				//if logged in show groups
+				//if logged in show class
 				//TODO check if person is teacher
 				if (isset($_SESSION["username"])){
 
 					$user = $_SESSION["username"];
 
-					$klassen = [];
-					$klassen['klas'] = [];
+					$userSchool = "";
 
-					$sql = "SELECT school, klas FROM users WHERE username='$user'";
+					//get school from teacher
+					$sql = "SELECT school FROM users WHERE username='$user'";
 
 					if (mysqli_query($conn, $sql)) {
-						//find school of teacher
+
 						$result = mysqli_query($conn, $sql);
 						$result = mysqli_fetch_assoc($result);
-						$school = $result['school'];
+						$userSchool = $result['school'];
 
-						$sql = "SELECT naam, username, klas FROM `users` WHERE school='$school' AND functie='leerling'";
+					} else {
+						echo "Error with sql execution, please report to admin </br>";
+					}
 
-						if (mysqli_query($conn, $sql)) {
+					$groepen = [];
+					$groepen['groep'] = [];
 
-							$result = mysqli_query($conn, $sql);
+					$sql = "SELECT school, groep FROM groepen WHERE school='$userSchool'";
 
-							if (mysqli_num_rows($result) > 0) {
-							    // output data of each row of names with class
+					if (mysqli_query($conn, $sql)) {
+						//load groups
+						$result = mysqli_query($conn, $sql);
+						$result = mysqli_fetch_assoc($result);
 
-							    while($row = mysqli_fetch_assoc($result)) {
-									$klas = $row["klas"];
+						if(mysqli_num_rows($result) > 0){
 
-									$naam = $row["naam"];
-									$username = $row["username"];
+							while($row = mysqli_fetch_assoc($result)) {
+								$Gnaam = $row["naam"];
+								$Glink = $row["link"];
 
-									//add userinfo to right class
-									$userinfo = ['naam'=>$naam, 'username'=>$username];
-
-									$klassen['klas'][$klas][] = $userinfo;
-
-							    }
-
-							} else {
-							    echo "0 results";
+								//add groups to array
+								$groepen['groep'][$Gnaam] = $Gnaam;
 							}
 
-							//How many classes are there?
-							$Nclasses = count($klassen['klas']);
+							//number of groups
+							$Ngroepen = count($groepen['groep']);
 
-							//put the classes in the right order
-							ksort($klassen['klas']);
+							//sort groups
+							ksort($groepen['groep']);
 
-							//Show me these (the nice way)
-							$AllClasses = array_keys($klassen['klas']);
+							//get group names
+							$AllGroups = array_keys($groepen['groep']);
 
-							for($i=0; $i < $Nclasses; $i++){
-								$CurrentClass = $AllClasses[$i];
+							//show these groups
+							for($i=0; $i<$Ngroepen; $i++){
+								$CurrentGroup = $AllGroups[$i];
 
-								$StudentsCurrentClass[] = $klassen['klas'][$CurrentClass];
+								$CurrentGroupName = $groepen['groep'][$CurrentGroup];
 
-								$NStudents = array_map("count", $StudentsCurrentClass);
-								$NStudentsCurrentClass = ($NStudents[$i]);
+								//TODO
+								$NMembersCurrentGroup = 1;
+
+								$klassen = 'H51';
 
 								echo'
+
 								<div class="class">
 									<!-- table header for this class-->
 									<div class="classHeader">
-										<span class="klas">'.$CurrentClass.'</span>
-										<span class="Nleerlingen">'.$NStudentsCurrentClass.' leerlingen </span>
+										<span class="groep">'.$CurrentGroup.'</span>
+										<span class="Nleden">'.$NMembersCurrentGroup.' groepsleden </span>
+										<span class="klassen">'.$klassen.'</span>
 										<span class="icons">
-											<span class="Mail image"><img src="../icons/mail.svg"/></span>
 											<span class="Arrow image"><img src="../icons/arrow.svg" class="arrow"/></span>
 										</span>
 									</div>
 
 									<!-- table content for this class-->
-									<div class="classContent">';
+									<div class="classContent">
 
+										<div class="groepBeschrijving">
+											//TODO
+											klhdkgsgjkajsdkgfhjdhgiuhjskcghniuwehrihgnjicdhnguhwepg
+										</div>';
 
-									for($j=0; $j<$NStudentsCurrentClass; $j++){
-										$Cstudent = $StudentsCurrentClass[$i][$j];
-										$CstudentName = $Cstudent['naam'];
-										$CstudentUsername = $Cstudent['username'];
+									for($j=0; $j<$NMembersCurrentGroup; $j++){
+										//TODO
+										$Cmember = 'Test';
+										$CmemberName = 'naam';
+										$CmemberKlas = 'klas';
+										$CmemberFunctie = 'functie';
 										$CstudentProgress = 0;
 										//TODO change icon style based on progres
 
 										echo '
 
-												<div class="row">
-													<span class="name">'.$CstudentName.'</span>
-													<span class="username">'.$CstudentUsername.'</span>
-													<span class="progress">icon</span>
-												</div>
+											<div class="row">
+												<span class="name">'.$CstudentName.'</span>
+												<span class="username">'.$CstudentUsername.'</span>
+												<span class="progress">icon</span>
+											</div>
 
 										';
 
 									}
 
-							echo '</div></div>';
+								echo '</div></div>';
+
 							}
 
 						} else {
-							echo "Geen klassen gevonden </br>";
+							echo "Geen groepen gevonden </br>";
 						}
 
 					} else {
-						echo "Uw gegevens zijn niet gevonden </br>";
+						echo "Error with sql execution, please report to admin </br>";
 					}
 
 				} else {

f4935b573f6137b3dd1e8b5d8b863d6dfea2a73b
Main <steeman.rene1@gmail.com>
2018-03-30 21:52:33 +0200

    Updated the logo and added the first part of the quiz that comes at the end of the chapter (bug with checkbox JS)

---
+++ design V2/pages/theorie/H1/quiz.php
@@ -0,0 +1,63 @@
+<?php
+	include('../../../components/headerChapter.php');
+?>
+
+<link rel="stylesheet" href="../../../css/quiz.min.css">
+<link rel="stylesheet" href="../../../scripts/UI.js">
+
+<body>
+
+	<div class="title-small">
+		<h2>
+			H1 quiz
+		</h2>
+	</div>
+
+	<div class="quiz-bar">
+		<!-- TODO show actual number -->
+		vraag 1/5
+	</div>
+
+	<div class="vraag">
+		<div class="vraagInhoud">
+			Wat is 1000111 binair in het hexadecimaal systeem?
+		</div>
+		<div class="antwoorden">
+			<ul>
+				<li>
+					<label class="container">113
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">80
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">71
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container" class="single-select-checkbox">4
+						<input type="radio">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+
+
+	</div>
+
+	<?php
+	include('../../../components/footerChapter.php');
+	?>
+
+</body>

f01be4f704248a735053ca26be5683f82097e6f0
Main <steeman.rene1@gmail.com>
2018-04-01 17:57:20 +0200

    Your highscore is now saved

--- design V2/scripts/quiz.php
+++ design V2/scripts/quiz.php
@@ -3,7 +3,7 @@
 	session_start();
 
 	//get and filter data
-	$user = $_SESSION["username"];
+	$username = $_SESSION["username"];
 	$hoofdstuk = $_POST['hoofdstuk'];
 	$antwoorden = $_POST['antwoorden'];
 
@@ -22,6 +22,58 @@
 		$cijfer = ((($punten * 10) / count($antwoorden)) * 9 + 10)/10;
 
 		echo 'Je hebt een '.$cijfer.' gehaald';
+
+		//get id for $username
+		$sql = "SELECT id FROM users WHERE username='$username'";
+
+		if (mysqli_query($conn, $sql)) {
+
+			$result = mysqli_query($conn, $sql);
+			$result = mysqli_fetch_assoc($result);
+			$userid = $result['id'];
+
+
+
+			//check if user already has a score for the quiz
+			$sql = "SELECT cijfer FROM quiz WHERE userid=$userid";
+
+			if (mysqli_query($conn, $sql)) {
+
+				$result = mysqli_query($conn, $sql);
+				//if no result is found than insert result
+				if (mysqli_num_rows($result) == 0){
+					$sql = "INSERT INTO quiz (userid, hoofdstuk, cijfer) VALUES ($userid, $hoofdstuk, $cijfer)";
+					if (mysqli_query($conn, $sql)) {
+						echo "\nHet cijfer is opgeslagen";
+					} else {
+						echo "SQL error report to admin";
+					}
+				} else {
+					$result = mysqli_fetch_assoc($result);
+					$Ccijfer = $result['cijfer'];
+
+					if($cijfer > $Ccijfer){
+						$sql = "UPDATE quiz SET cijfer=$cijfer WHERE userid=$userid AND hoofdstuk=$hoofdstuk";
+						if (mysqli_query($conn, $sql)) {
+							echo "\nHet cijfer is opgeslagen";
+						} else {
+							echo "SQL error report to admin";
+						}
+					} else {
+						echo "\nDit is niet jouw hoogste score, het zal niet worden opgeslagen";
+					}
+
+
+				}
+
+			} else {
+				echo "SQL error report to admin";
+			}
+
+		} else {
+			echo "SQL error report to admin";
+		}
+
 	}
 
 

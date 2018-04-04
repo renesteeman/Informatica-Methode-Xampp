bb4c04fcaa47f45eba6e633a719fe3898953ef5f
Main <steeman.rene1@gmail.com>
2018-04-04 21:55:02 +0200

    Now displays data from DB

--- design V2/pages/planner.php
+++ design V2/pages/planner.php
@@ -1,5 +1,6 @@
 <?php
 	include('../components/headerGeneral.php');
+	include('../scripts/DB_connect.php');
 ?>
 
 <link rel="stylesheet" href="../css/planner.min.css">
@@ -46,6 +47,110 @@
 			</div>
 
 
+
+			<?php
+				if (isset($_SESSION["username"])){
+
+					$user = $_SESSION["username"];
+
+					$klassen = [];
+					$klassen['klas'] = [];
+
+					$sql = "SELECT school, klas, functie FROM users WHERE username='$user'";
+
+					if (mysqli_query($conn, $sql)) {
+						//find teacher info
+						$result = mysqli_query($conn, $sql);
+						$result = mysqli_fetch_assoc($result);
+						$Pschool = $result['school'];
+						$Pklas = $result['klas'];
+						$Pfunctie = $result['functie'];
+
+						$sql = "SELECT titel, beschrijving, datum FROM planner WHERE school='$Pschool'";
+
+						if (mysqli_query($conn, $sql)) {
+							//get planner info
+							$result = mysqli_query($conn, $sql);
+							$Nitems = mysqli_num_rows($result);
+
+							if (mysqli_num_rows($result) > 0) {
+							    // output data of each row
+							    while($row = mysqli_fetch_assoc($result)) {
+									$Ititel = $row['titel'];
+									$Ibeschrijving = $row['beschrijving'];
+									$Idatum = date('d/m', strtotime($row['datum']));
+
+									if($Pfunctie == "docent"){
+										echo '
+										<div class="item">
+											<div class="itemHeader">
+												<!-- table header for this class-->
+												<div class="itemHeaderContent">
+													<span class="datum">'.$Idatum.'</span>
+													<span class="naam">'.$Ititel.'</span>
+													<span class="icons">
+														<span class="Arrow image"><img src="../icons/arrow.svg" class="arrow"/></span>
+														<span class="Edit image"><img src="../icons/edit.svg" class="arrow"/></span>
+													</span>
+												</div>
+
+												<!-- table content for this class-->
+												<div class="itemContent">
+
+													<div class="itemInhoud">
+														<span class="itemBeschrijving">
+															'.$Ibeschrijving.'
+														</span>
+													</div>
+												</div>
+											</div>
+										</div>
+										';
+
+									}
+
+									if($Pfunctie == "leerling"){
+										echo '
+										<div class="item">
+											<div class="itemHeader">
+												<!-- table header for this class-->
+												<div class="itemHeaderContent">
+													<span class="datum">'.$Idatum.'</span>
+													<span class="naam">'.$Ititel.'</span>
+													<span class="icons">
+														<span class="Arrow image"><img src="../icons/arrow.svg" class="arrow"/></span>
+													</span>
+												</div>
+
+												<!-- table content for this class-->
+												<div class="itemContent">
+
+													<div class="itemInhoud">
+														<span class="itemBeschrijving">
+															'.$Ibeschrijving.'
+														</span>
+													</div>
+												</div>
+											</div>
+										</div>
+										';
+									}
+								}
+							}
+
+						} else {
+							echo "SQL error, report to admin";
+						}
+
+
+					} else {
+						echo "SQL error, report to admin";
+					}
+				} else {
+					echo "U bent niet ingelogd";
+				}
+
+			?>
 
 		</div>
 	</div>

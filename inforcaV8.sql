4477d21219497525bc6b9f3775410459a3a789fa
Main <steeman.rene1@gmail.com>
2018-04-08 18:18:58 +0200

    new DB version

--- inforcaV8.sql
+++ inforcaV8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 23 mrt 2018 om 18:40
+-- Gegenereerd op: 08 apr 2018 om 18:17
 -- Serverversie: 10.1.28-MariaDB
 -- PHP-versie: 7.1.11
 
@@ -44,7 +44,38 @@ INSERT INTO `groepen` (`id`, `naam`, `beschrijving`, `link`, `school`) VALUES
 (22, 'bcd', 'hvrggcsdfvhfg', 'link', 'Group'),
 (24, 'test', 'ABC', 'giethup', 'Group'),
 (25, 'test groep 2', 'dhjgrtuybkvttiydgthidut65e79 tdygighc', 'giethup', 'Group'),
-(26, 'DELETE', '', '', 'Group');
+(26, 'idfosuyhuipsfhoujsdfgopgju', 'qdefrtklsfrtjjhml;kedfjihujswet8io5q', 'https://github.com/marketplace/category/project-ma', 'Group'),
+(27, 'demo2', 'demo groep', '', 'Group'),
+(28, 'demonstratie', 'demonstratie', 'https://github.com/renesteeman/Informatica-Methode', 'Group'),
+(29, 'asdf', 'asdf', '', 'Group'),
+(30, 'adsfvcjjerijrhjk', 'adsfvcjjerijrhjk', '', 'Group'),
+(31, 'asdvcxbwery', 'asdvcxbwery', '', 'Group');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `planner`
+--
+
+CREATE TABLE `planner` (
+  `id` int(11) NOT NULL,
+  `titel` text COLLATE utf8_bin NOT NULL,
+  `beschrijving` text COLLATE utf8_bin NOT NULL,
+  `progressie` text COLLATE utf8_bin,
+  `school` text COLLATE utf8_bin NOT NULL,
+  `klas` text COLLATE utf8_bin,
+  `datum` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `planner`
+--
+
+INSERT INTO `planner` (`id`, `titel`, `beschrijving`, `progressie`, `school`, `klas`, `datum`) VALUES
+(11, 'gjhk', 'ghf', 'H5, H2, H3, ', 'Group', 'H52', '2018-10-06'),
+(12, 'testV2', 'test4', 'H1, H5, ', 'Group', 'H52', '2018-05-04'),
+(14, 'kasdf', 'cvbjm', 'H1, H4, ', 'Group', 'A52', '2018-06-07'),
+(15, 'sdaf', 'zxcg', 'H2, H6, ', 'Group', 'A42', '2018-02-07');
 
 -- --------------------------------------------------------
 
@@ -70,7 +101,27 @@ CREATE TABLE `progressie` (
 --
 
 INSERT INTO `progressie` (`userid`, `H1`, `H2`, `H3`, `H4`, `H5`, `H6`, `H7`, `B1`, `B2`) VALUES
-(79, '110110', '01100', '010', NULL, NULL, NULL, NULL, '1110', '');
+(122, '511000', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `hoofdstuk` int(2) NOT NULL,
+  `cijfer` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `quiz`
+--
+
+INSERT INTO `quiz` (`id`, `userid`, `hoofdstuk`, `cijfer`) VALUES
+(4, 122, 1, 10);
 
 -- --------------------------------------------------------
 
@@ -106,27 +157,27 @@ INSERT INTO `users` (`id`, `naam`, `username`, `password`, `school`, `functie`, 
 (79, NULL, '6335790964', '$2y$10$48YD6bAB0UScfy5maDaziur8n4VURTpUOCd7gCJHbsVdbufNpduA2', 'Group', 'docent', '', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
 (80, NULL, '8025112436', '$2y$10$STz4aUpLKQHkGlqXTOmSveg8ntB1h2L91/SEvK8or6MbJS1kdFoBq', 'test', 'leerling', '', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
 (81, NULL, '2884153775', '$2y$10$e7Oyj/rNcJEe5PduJfKIDu2xm22S7AT9Qp4bfdw8B.KtHdK9j6phC', 'test', 'leerling', '', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
-(102, 'DEMO', '9527608045', '$2y$10$d/wwplYbNLrXP839nJomNe/As01dT/z3PDv.l92bQsCLIXy25SAhK', 'Group', 'leerling', 'H53', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
+(102, 'DEMO', '9527608045', '$2y$10$d/wwplYbNLrXP839nJomNe/As01dT/z3PDv.l92bQsCLIXy25SAhK', 'Group', 'leerling', 'H53', '2018-03-02', '2019-03-02', '', NULL, NULL),
 (103, '1', '9598311809', '$2y$10$AeWv9I33QLUYKy6vT7C8bujqm9s.fqgf9mHiLGbZIeUztfwdPBipK', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
 (104, 'test', '9441641450', '$2y$10$GODquEWJHo1Q2Bb66r9uTOPD2Tq4PRyqVsbQq55wi60wUh7sc/ddK', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
 (105, '2', '2074786768', '$2y$10$Fx0b8SoQWkbVWA3dQyYXmuYKNXHAk1hHojKuxXS6bCLXJh/sGtxgi', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'demo2', NULL, NULL),
 (106, '3', '4993318373', '$2y$10$Yl.RJV0fE1jkDBiy3Zdm9OV1Dy6kTuRAx8upYddzzqqLCmLbMGo5q', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
 (107, 'Luc', '0587908063', '$2y$10$Gf1doHas9AOwveMZUVTnBeYubmVY745a9GnQ9HAMbvhnn6mv.LOWm', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
 (108, 'Sam', '0818762595', '$2y$10$p5SklbbAVZM23ym31B8IRuq.3IYHMaHBWgO1VPF2n9TER6PWyi7sO', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'demo2', NULL, NULL),
-(109, 'DEMO2', '9651670092', '$2y$10$elndrVTL3s6cHXke/uEMw.AcCMw5C5xalrnUMBxXlT/Cwd4UTjcVC', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
+(109, 'DEMO2', '9651670092', '$2y$10$elndrVTL3s6cHXke/uEMw.AcCMw5C5xalrnUMBxXlT/Cwd4UTjcVC', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'asdvcxbwery', NULL, NULL),
 (110, 'demo', '5104488085', '$2y$10$SEUts6xLBOLF4hSedRPt0.qnGD3QhFKt/Ts6U0nPKRiVqkR935.Uu', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'demo2', NULL, NULL),
-(111, 'Person', '1307791270', '$2y$10$i2Il55jkOYsXIJW//IOZW.WG4cmFJMm2sxxL5iNBSZBOUcp0b6ZZK', 'Group', 'leerling', 'H54', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(112, 'asdf', '2611372284', '$2y$10$1FwkSuzzebO0uSzvigVh/.aBPNrYCxcMPLlt99GgpfApidY71iSGK', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(113, 'Person', '4550075293', '$2y$10$PLkHusGOljqQBP3aOlEeu.THW43T0du9LVNJMfyan7llqdUvZIP9m', 'Group', 'leerling', 'A51', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(114, 'Person', '6054234068', '$2y$10$Tg1wexChdzBSZkq0m86MAODHAcLJMkTyeb1uiUVYx3TJSFv7XUUl2', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(115, 'Person', '7675263504', '$2y$10$0gNu9utdLopjKnGUKB90AO5pZ2kIqlIdeXOySI4bdL5i./vpiiMfq', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(116, 'utyonhjgk', '1544673883', '$2y$10$M8o92F3SWuDviQFrs45YMOINZ5trHo4c.XF.ucUFoFB.L8t/FAvUK', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'demo2', NULL, NULL),
-(117, 'Person', '8411075718', '$2y$10$DtWbcLzzwdoOFuDVgdKu1eh1KVN/0d9tl4kDTY8yE/TUCgKdDJU.K', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(118, 'Person', '4839411746', '$2y$10$S22xdk.opJiBEbjtQPhgXOn8hGEFj2NnQY1QZ0BeRjOJ1bm4yE0ie', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(119, 'Person', '6546385045', '$2y$10$LQ7Z1okIDbaHf8pYe.CDw.Ku3XQVvJWucjhPrnmHp/WvZSJ49f8hy', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(120, 'i4323', '3837331551', '$2y$10$MqSSc9PRduwwiJK.fDQb9eixKH7p1rhXNgWZyp5F0xkxSDeVIJfNy', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(121, 'Person', '9456448199', '$2y$10$xWlQlAuyRZr4y9ekvAVJTejbfHvJXpZbUnZflL76jPllsP4zYfPoa', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'test', NULL, NULL),
-(122, 'Docent', 'docent', '$2y$10$.iP7CYRVbQBGS98u/9G8VOPr67dgUJklK98BgO7y4AqUQSLD4doRK', 'Group', 'docent', 'H52', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
+(111, 'Person', '1307791270', '$2y$10$i2Il55jkOYsXIJW//IOZW.WG4cmFJMm2sxxL5iNBSZBOUcp0b6ZZK', 'Group', 'leerling', 'H54', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(112, 'asdf', '2611372284', '$2y$10$1FwkSuzzebO0uSzvigVh/.aBPNrYCxcMPLlt99GgpfApidY71iSGK', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(113, 'Person', '4550075293', '$2y$10$PLkHusGOljqQBP3aOlEeu.THW43T0du9LVNJMfyan7llqdUvZIP9m', 'Group', 'leerling', 'A51', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(114, 'Person', '6054234068', '$2y$10$Tg1wexChdzBSZkq0m86MAODHAcLJMkTyeb1uiUVYx3TJSFv7XUUl2', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(115, 'Person', '7675263504', '$2y$10$0gNu9utdLopjKnGUKB90AO5pZ2kIqlIdeXOySI4bdL5i./vpiiMfq', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(116, 'utyonhjgk', '1544673883', '$2y$10$riK4EvLSpKyxMo3pQDEBCODfi4awMA7/5bbrzo6oCUpu48JDkZD42', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', 'demo2', NULL, 'steeman.rene1@gmail.com'),
+(117, 'Person', '8411075718', '$2y$10$DtWbcLzzwdoOFuDVgdKu1eh1KVN/0d9tl4kDTY8yE/TUCgKdDJU.K', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(118, 'Person', '4839411746', '$2y$10$S22xdk.opJiBEbjtQPhgXOn8hGEFj2NnQY1QZ0BeRjOJ1bm4yE0ie', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(119, 'Person', '6546385045', '$2y$10$LQ7Z1okIDbaHf8pYe.CDw.Ku3XQVvJWucjhPrnmHp/WvZSJ49f8hy', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(120, 'i4323', '3837331551', '$2y$10$MqSSc9PRduwwiJK.fDQb9eixKH7p1rhXNgWZyp5F0xkxSDeVIJfNy', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(121, 'Person', '9456448199', '$2y$10$xWlQlAuyRZr4y9ekvAVJTejbfHvJXpZbUnZflL76jPllsP4zYfPoa', 'Group', 'leerling', 'H52', '2018-03-02', '2019-03-02', '', NULL, NULL),
+(122, 'Niet in gebruik', 'docent', '$2y$10$.iP7CYRVbQBGS98u/9G8VOPr67dgUJklK98BgO7y4AqUQSLD4doRK', 'Group', 'docent', 'H52', '2018-03-02', '2019-03-02', NULL, NULL, NULL),
 (123, 'Iemand', '9485091361', '$2y$10$2HApZfG0Xbm95.FDQ6NAL./IiF6q9tn.zPHd.buy7Qjt6bspSHLL6', 'Group', 'leerling', 'H51', '2018-03-03', '2019-03-03', NULL, NULL, NULL),
 (124, 'Iemand', '1600554899', '$2y$10$xy.wLksSgorc.EeoOppT2.0qm0JzQfZuo4SHNByjUYDJYU9gzKLhW', 'Group', 'leerling', 'H51', '2018-03-03', '2019-03-03', NULL, NULL, NULL),
 (125, 'Iemand', '0585668016', '$2y$10$OARCyRHnZmntei8Yabx7n.3XDXRT2.R3IN9lhlyXmPo5xPlTnys6a', 'Group', 'leerling', 'H51', '2018-03-03', '2019-03-03', NULL, NULL, NULL),
@@ -146,7 +197,8 @@ INSERT INTO `users` (`id`, `naam`, `username`, `password`, `school`, `functie`, 
 (139, NULL, '6591523349', '$2y$10$itVDmZZCR/4BoZ9iJfaE4uJ9D.EgBJqbNeg5f9hWgu5y0A9zlZRI6', 'Testers college', 'leerling', '', '2018-03-04', '2019-03-04', '1', NULL, NULL),
 (140, NULL, '5187906434', '$2y$10$PHyrZGdyvcgxGVLEI0Gc3eyClRX6.02sMR1wJQ3oazjBJAbRYbG9m', 'Testers college', 'leerling', '', '2018-03-04', '2019-03-04', '1', NULL, NULL),
 (141, NULL, '0190244192', '$2y$10$nqVebh79ftMyIzcqLu/4X.DI/BNbmIAe/F6oChd3fi845PTYGc9HK', 'Testers college', 'leerling', '', '2018-03-04', '2019-03-04', '1', NULL, NULL),
-(142, NULL, '6310850604', '$2y$10$u0Ccu3xORo/kBIvhlw4RhuuMshyiALufsS26Ro1UTBn802tiLZRnS', 'Testers college', 'leerling', '', '2018-03-04', '2019-03-04', '1', NULL, NULL);
+(142, NULL, '6310850604', '$2y$10$u0Ccu3xORo/kBIvhlw4RhuuMshyiALufsS26Ro1UTBn802tiLZRnS', 'Testers college', 'leerling', '', '2018-03-04', '2019-03-04', '1', NULL, NULL),
+(143, 'test', 'leerling', '$2y$10$jjgFea2L47iZsNSeNYvBZ.bZWe8R6DpjR/dM5xVg1gxu4WTI23MUq', 'Group', 'leerling', 'H51', '2018-04-05', '2019-04-05', NULL, NULL, NULL);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -159,10 +211,22 @@ ALTER TABLE `groepen`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexen voor tabel `planner`
+--
+ALTER TABLE `planner`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexen voor tabel `progressie`
 --
 ALTER TABLE `progressie`
   ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexen voor tabel `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `users`
@@ -178,13 +242,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `groepen`
 --
 ALTER TABLE `groepen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT voor een tabel `planner`
+--
+ALTER TABLE `planner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT voor een tabel `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

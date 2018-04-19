c0760580281f6c7f77450d733a7d1e1e879302c4
Main <steeman.rene1@gmail.com>
2018-04-19 09:39:33 +0200

    logic part 1

--- design V2/scripts/changeClassFront.php
+++ design V2/scripts/changeClassFront.php
@@ -15,22 +15,141 @@
 
 	<div class="title">
 		<h2>
-			Klas wijzigen
+			Leerlingen informatie wijzigen
 		</h2>
+	</div>
+
+	<div class="bar">
+		<h3>
+			Klas wijzigen
+		</h3>
 	</div>
 
 	<div class="main">
 		<form class="accountSettingsForm" method="post" action="../scripts/updateAccount.php" accept-charset="UTF-8">
+
 			<ul>
-				Klas:
-				<div class="klasSelector">
-					<select>
-						<option value="volvo">H51</option>
-						<option value="saab">A51</option>
-						<option value="opel">Opel</option>
-						<option value="audi">Audi</option>
-					</select>
-				</div>
+
+
+			<?php
+				//if logged in show class
+				if (isset($_SESSION["username"])){
+
+					$user = $_SESSION["username"];
+
+					$klassen = [];
+					$klassen['klas'] = [];
+
+					$sql = "SELECT school, functie FROM users WHERE username='$user'";
+
+					if (mysqli_query($conn, $sql)) {
+						//find school of teacher
+						$result = mysqli_query($conn, $sql);
+						$result = mysqli_fetch_assoc($result);
+						$school = $result['school'];
+						$functie = $result['functie'];
+
+						if($functie != 'docent'){
+							echo "U bent geen docent";
+						} else {
+							$sql = "SELECT naam, klas FROM `users` WHERE school='$school' AND functie='leerling'";
+
+							if (mysqli_query($conn, $sql)) {
+
+								$result = mysqli_query($conn, $sql);
+
+								if (mysqli_num_rows($result) > 0) {
+									// output data of each row of names with class
+
+									while($row = mysqli_fetch_assoc($result)) {
+										$klas = $row["klas"];
+										$naam = $row["naam"];
+
+										//save info from user
+										$userinfo = ['naam'=>$naam, 'klas'=>$klas];
+
+										//save info
+										$klassen['klas'][$klas][] = $userinfo;
+
+									}
+
+							   } else {
+								   echo "0 results";
+							   }
+
+								//How many classes are there?
+								$Nclasses = count($klassen['klas']);
+
+								//put the classes in the right order
+								ksort($klassen['klas']);
+
+								//Show me these (the nice way)
+								$AllClasses = array_keys($klassen['klas']);
+
+								echo 'Klas: <div class="klasSelector"><select>';
+
+
+								for($i=0; $i < $Nclasses; $i++){
+									$CurrentClass = $AllClasses[$i];
+
+									echo'
+									<option value="'.$CurrentClass.'">'.$CurrentClass.'</option>
+									';
+
+								}
+
+								echo '</select></div>';
+
+							} else {
+								echo "SQL error, report to admin";
+							}
+						}
+					} else {
+						echo "SQL error, report to admin";
+					}
+			   } else {
+				   echo 'U bent niet ingelogd';
+			   }
+
+			   /*
+				  for($j=0; $j<$NStudentsCurrentClass; $j++){
+					  $Cstudent = $StudentsCurrentClass[$i][$j];
+					  $CstudentName = $Cstudent['naam'];
+					  $CstudentGroupName = $Cstudent['group_name'];
+					  $CstudentGroupRole= $Cstudent['group_role'];
+					  $ConSchedule = $Cstudent['onSchedule'];
+					  $Caverage = $Cstudent['gemiddeldePunt'];
+
+					  if($CstudentGroupName==""){
+						  $CstudentGroupName = "zit niet in een groep";
+						  $CstudentGroupRole = "";
+					  }
+
+					  echo '
+
+					  <div class="row">
+						  <span class="name">'.$CstudentName.'</span>
+						  <span class="groepnaam">'.$CstudentGroupName.'</span>
+						  <span class="groepsrol">'.$CstudentGroupRole.'</span>
+						  <span class="gemiddelde">'.$Caverage.'</span>';
+
+					  if($ConSchedule){
+						  echo '
+						  <span class="progressie">
+							  <span class="onSchedule"></span>
+						  </span>
+						  ';
+					  } else {
+						  echo '
+						  <span class="progressie">
+							  <span class="notSchedule"></span>
+						  </span>
+						  ';
+					  } */
+
+			?>
+
+
 
 				<div class="leerlingSelector">
 					Leerlingen:

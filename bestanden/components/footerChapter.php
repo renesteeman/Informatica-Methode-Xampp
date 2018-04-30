1e2772ac65d2542be2d0726f1032af62ca35afd2
Main <steeman.rene1@gmail.com>
2018-04-30 15:47:54 +0200

    4.4

--- bestanden/components/footerChapter.php
+++ bestanden/components/footerChapter.php
@@ -5,7 +5,13 @@
 			Neem gerust contact op voor vragen, suggesties, adviezen en overigen. U mag binnen 24 een antwoord verwachten via de mail.
 		</div>
 		<div class="contact-info">
-			<a id="phone" href="tel:+045622155216">06-22155216</a>
+			<?php
+				if (isset($_SESSION["username"])){
+					if ($_SESSION["functie"] == 'docent'){
+						echo '<a id="phone" href="tel:+045622155216">06-22155216</a>';
+					}
+				}
+			?>
 			<a id="mail" href="mailto:koffieandcode@gmail.com?SUBJECT=Inforca">koffieandcode@gmail.com</a>
 		</div>
 	</div>
--- bestanden/components/headerChapter.php
+++ bestanden/components/headerChapter.php
@@ -1,5 +1,4 @@
 <?php
-session_save_path('../../../tmp');
 session_start();
 ?>
 

38de2c8fd34621606398c194abbd688f2b07da20
Main <steeman.rene1@gmail.com>
2018-05-13 19:44:35 +0200

    new icon system

--- bestanden/components/headerChapter.php
+++ bestanden/components/headerChapter.php
@@ -1,6 +1,49 @@
 <?php
 session_save_path('../../../tmp');
 session_start();
+
+include('../../../scripts/DB_connect.php');
+
+//check if the person is loged in
+if (!isset($_SESSION["username"])){
+	$_SESSION['ErrorNotLogedIn'] = 1;
+	header('Location: ../../index.php');
+}
+
+//check if account is still valid
+function AccountValid(){
+
+	//needed to connect inside the function
+	global $conn;
+
+	$user = $_SESSION["username"];
+	$sql = "SELECT expire_date FROM users WHERE username='$user'";
+
+	if (mysqli_query($conn, $sql)) {
+		//find teacher info
+		$result = mysqli_query($conn, $sql);
+		$result = mysqli_fetch_assoc($result);
+		$expire_date = $result['expire_date'];
+	} else {
+		echo "SQL error, report to admin";
+	}
+
+	if($expire_date>date("Y-m-d")){
+		$valid = 1;
+	} else {
+		$valid = 0;
+	}
+
+	return $valid;
+}
+
+//check if the account hasn't expired and if it has, than redirect
+if(AccountValid()){
+	$_SESSION['ErrorInvalidAccount'] = 0;
+} else {
+	$_SESSION['ErrorInvalidAccount'] = 1;
+	header('Location: ../../index.php');
+}
 ?>
 
 <!DOCTYPE HTML>
@@ -24,7 +67,25 @@ session_start();
 	<link rel="stylesheet" href="../../../css/paragraph.min.css">
 
     <title>Inforca</title>
-	<link rel="icon" type="image/png" href="../../../icons/icon64.png"/>
+
+	<!-- icon -->
+	<link rel="apple-touch-icon" sizes="57x57" href="../icons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="../icons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../icons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../icons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../icons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="../icons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="../icons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="../icons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../icons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="../icons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="../icons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../icons/favicon-16x16.png">
+	<link rel="manifest" href="../icons/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="../icons/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 
 <head>
 
@@ -155,51 +216,3 @@ session_start();
 	</div>
 
 </header>
-
-<?php
-
-	include('../../../scripts/DB_connect.php');
-
-	//check if the person is loged in
-	if (!isset($_SESSION["username"])){
-		$_SESSION['ErrorNotLogedIn'] = 1;
-		//header('Location: ../../index.php');
-	}
-
-	//check if account is still valid
-	function AccountValid(){
-
-		//needed to connect inside the function
-		global $conn;
-
-		$user = $_SESSION["username"];
-		$sql = "SELECT expire_date FROM users WHERE username='$user'";
-
-		if (mysqli_query($conn, $sql)) {
-			//find teacher info
-			$result = mysqli_query($conn, $sql);
-			$result = mysqli_fetch_assoc($result);
-			$expire_date = $result['expire_date'];
-		} else {
-			echo "SQL error, report to admin";
-		}
-
-		if($expire_date>date("Y-m-d")){
-			$valid = 1;
-		} else {
-			$valid = 0;
-		}
-
-		return $valid;
-	}
-
-	//check if the account hasn't expired and if it has, than redirect
-	if(AccountValid()){
-		$_SESSION['ErrorInvalidAccount'] = 0;
-	} else {
-		$_SESSION['ErrorInvalidAccount'] = 1;
-		//header('Location: ../../index.php');
-	}
-
-
-?>

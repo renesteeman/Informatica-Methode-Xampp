dedc3b75f934574b82da1c53dea94b9913491443
Main <steeman.rene1@gmail.com>
2018-05-14 21:58:44 +0200

    *bugged* part 4

--- bestanden/scripts/login.php
+++ bestanden/scripts/login.php
@@ -15,7 +15,7 @@
 	$password = mysqli_real_escape_string($conn, check_input($_POST['password']));
 
 	//get password for $username
-	$sql = "SELECT password, functie, naam FROM users WHERE username='$username'";
+	$sql = "SELECT password, functie, naam, NFailedLogins, LFailedLogin FROM users WHERE username='$username'";
 
 	if (mysqli_query($conn, $sql)) {
 
@@ -26,60 +26,50 @@
 		$naam = $result['naam'];
 
 		//check if this account isn't blocked for security reasons
-		//load failed logins
-		$sql = "SELECT NFailedLogins, LFailedLogin FROM users WHERE username='$username'";
+		$NFailedLogins = $result['NFailedLogins'];
+		$LFailedLogin = $result['LFailedLogin'];
 
-		//get current info in order to show a 'preview'
-		if(mysqli_query($conn, $sql)) {
-			$result = mysqli_query($conn, $sql);
-			$result = mysqli_fetch_assoc($result);
+		if($NFailedLogins > 3){
+			if(strtotime($LFailedLogin) > (strtotime($NLFailedLogin)) - strtotime("+1 day")){
+				echo "\n It's time for a captcha since you had the wrong login 3(+) times in a row within the last 24h\n";
+			}
+		} else {
+			//continue login attempt
 
-			$NFailedLogins = $result['NFailedLogins'];
-			$LFailedLogin = $result['LFailedLogin'];
+			//check psw
+			if(password_verify($password, $rightpsw)){
+				//if it's right than login
 
-			if($NFailedLogins > 3){
-				if(strtotime($LFailedLogin) > (strtotime($NLFailedLogin)) - strtotime("+1 day")){
-					echo "\n It's time for a captcha since you had the wrong login 3(+) times in a row within the last 24h\n";
-				}
-			} else {
-				//continue login attempt
+				//start session with username
+				$_SESSION["username"] = $username;
+				$_SESSION["name"] = $naam;
+				$_SESSION["functie"] = $functie;
 
-				//check psw
-				if(password_verify($password, $rightpsw)){
-					//if it's right than login
-					
-					//start session with username
-					$_SESSION["username"] = $username;
-					$_SESSION["name"] = $naam;
-					$_SESSION["functie"] = $functie;
-
-					$sql = "UPDATE users SET NFailedLogins='0' WHERE username='$username'";
-					if(mysqli_query($conn, $sql)) {
-					} else {
-						echo "SQL error, please report to admin";
-					}
-
-					mysqli_close($conn);
+				$sql = "UPDATE users SET NFailedLogins='0' WHERE username='$username'";
+				if(mysqli_query($conn, $sql)) {
+					echo "updated";
 				} else {
-					//if the password is wrong
-					echo "\nIncorrecte login gegevens";
+					echo "SQL error, please report to admin";
+				}
 
-					$NNFailedLogins = $NFailedLogins + 1;
-					$NLFailedLogin = date('Y-m-d H:i:s');
+				mysqli_close($conn);
+			} else {
+				//if the password is wrong
+				echo "\nIncorrecte login gegevens";
 
-					//save failed logins
-					$sql = "UPDATE users SET NFailedLogins='$NNFailedLogins', LFailedLogin='$NLFailedLogin' WHERE username='$username'";
+				$NNFailedLogins = $NFailedLogins + 1;
+				$NLFailedLogin = date('Y-m-d H:i:s');
 
-					//get current info in order to show a 'preview'
-					if(mysqli_query($conn, $sql)) {
-						echo 'saved failed login';
-					}
+				//save failed logins
+				$sql = "UPDATE users SET NFailedLogins='$NNFailedLogins', LFailedLogin='$NLFailedLogin' WHERE username='$username'";
 
+				//get current info in order to show a 'preview'
+				if(mysqli_query($conn, $sql)) {
+					echo 'Saved failed login';
 				}
 
 			}
-		} else {
-			echo "SQL error, please report to admin";
+
 		}
 
 	} else {

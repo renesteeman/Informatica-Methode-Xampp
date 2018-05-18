047cc275caadfb56fc8e45f660ed7baa602f9a54
Main <steeman.rene1@gmail.com>
2018-05-18 22:13:27 +0200

    models V2 (first half)

--- bestanden/pages/theorie/H1/p4.php
+++ bestanden/pages/theorie/H1/p4.php
@@ -54,22 +54,34 @@
 			</p>
 
 			<p>
+				<img src="./afbeeldingen/CPU.png" class="theorieImage small" />
+
 				Het hart van de computer is de CPU, Central Processing Unit. Dit is een chip van normaalgesproken enkelen centimeters groot waarop alle algemene instructies worden uitgevoerd met behulp van de eerder besproken gates en dus het heen en weer halen van binaire getallen. Tegenwoordig heeft een CPU meerdere cores, dit wil zeggen dat het meerdere opdrachten tegelijk kunnen worden uitgevoerd. Elke core werkt dan als een soort mini CPU, maar dan niet met alle onderdelen die de gehele CPU heeft. De CPU heeft namelijk naast alleen de onderdelen voor het rekenen ook andere onderdelen, een voorbeeld is cache opslag. Cache is een vorm van hele snelle opslag, maar vaak met kleine capiciteit (vaak maar enkele MB). De snelheid van een CPU hangt van meerdere zaken af, voornamelijk de IPC, frequentie, cache en de hoeveelheid cores. De IPC is het aantal instructies dat per clock uitgevoerd kan worden. Een clock is een cyclus van instructies, zoals haal waardes van x naar y of voeg x toe aan y. De frequentie geeft aan hoeveel clocks er in een seconde uitgevoerd worden.
 			</p>
 
 			<p>
+				<img src="./afbeeldingen/RAM.png" class="theorieImage small" />
+
 				Het RAM, random accessible memory, slaat alle informatie op die snel beschikbaar moet zijn. Zaken zoals het OS en programma’s worden hierop gezet, zodat de CPU niet steeds verzoeken hoeft te doen om gegevens op een schijf op te vragen en ze dus veel sneller kan krijgen. Hoe meer ram, des te meer er opgeslagen kan worden in dit snelle geheugen.
 			</p>
 
 			<p>
+				<img src="./afbeeldingen/MOBO.png" class="theorieImage small" />
+
 				Het moederbord is een soort hub waar alles op aangesloten is: de CPU, GPU, schijven (opslag), usb poorten, internet en nog een boel andere zaken zitten allemaal erop aangesloten. Het moederbord zorgt er dus voor dat alle onderdelen met elkaar kunnen communiceren.
 			</p>
 
 			<p>
+				<img src="./afbeeldingen/GPU.png" class="theorieImage small" />
+
 				De GPU heeft heel veel cores, maar weer een lagere frequentie ten opzichte van de CPU en het is lastiger om er bepaalde instructies op uit te voeren. De GPU kan dus vooral veel eenvoudige taken tegelijkertijd uitvoeren. Dit maakt het erg handig voor het laden van beelden en dus voor bijvoorbeeld het spelen van spellen. De GPU kan echter ook andere taken uitvoeren, maar deze zijn vrij geavanceerd. Denk hierbij bijvoorbeeld aan het trainen van zelflerende programma’s.
 			</p>
 
 			<p>
+				<img src="./afbeeldingen/HDD.png" class="theorieImage small inline" />
+				<img src="./afbeeldingen/SSD.png" class="theorieImage small inline" />
+				</br>
+
 				Er zijn meerdere soorten schijven. Deze zijn voornamelijk op te delen in twee type schijven; de SSD (Solid State Drive) en de HDD (harddisk drive). De SSD werkt met cellen die maar een aantal keer beschreven kunnen worden, terwijl een HDD werkt met ladingen aanbrengen op een ronddraaiende schijf. Het voordeel van een SSD is dat hij t.o.v. een HDD veel sneller is en dat als het kapot gaat de data vaak leesbaar blijft.
 
 				Het nadeel is dat op dit moment SSD's nog wel prijzig zijn vergeleken met een HDD. Een voordeel van een HDD is dat deze voor weinig geld veel informatie op kunnen slaan. De prijs per GB voor een SSD bedraagen nu (in 2018) rond de €0,268, terwijl de prijs per GB voor een HDD zo rond de €0,028 ligt.

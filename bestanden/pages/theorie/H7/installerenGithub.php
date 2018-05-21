e0f865968db11c91616c1eeb661f14ab02779804
Main <steeman.rene1@gmail.com>
2018-05-21 20:09:24 +0200

    H7

---
+++ bestanden/pages/theorie/H7/installerenGithub.php
@@ -0,0 +1,66 @@
+<?php
+	include('../../../components/headerChapter.php');
+?>
+
+<body>
+
+	<div class="title-small">
+		<h2>
+			H7 Github installeren
+		</h2>
+	</div>
+
+	<div class="installation-bar">
+		<h3>Github</h3>
+	</div>
+
+	<div class="theorie">
+		<div class="bar-s">
+			<h3>
+				Installatie instructies
+			</h3>
+		</div>
+
+		<div class="theorie-content">
+			<pre>
+Als eerste is een account nodig op <a href="https://github.com">https://github.com</a>, deze kun je nu eenvoudig aanmaken.
+
+Vervolgens is het tijd voor een GUI om het gebruik een stuk makkelijker te maken. Er zijn veel varianten van beschikbaar, als voorbeeld nemen we sourcetree. Je kunt het downloaden op <a href="https://www.sourcetreeapp.com">https://www.sourcetreeapp.com</a>.
+			</pre>
+
+			<ol>
+				<li>
+					Open de installer
+				</li>
+				<li>
+					Accepteer de license agreement
+				</li>
+				<li>
+					Klik op atlassian account
+				</li>
+				<li>
+					Creëer een atlassian account of login met google
+				</li>
+				<li>
+					Installeer git en mercurial
+				</li>
+				<li>
+					Klik ‘no’ als gevraagd wordt voor een global ignore file
+				</li>
+				<li>
+					Klik ‘no’ voor de SSH key
+				</li>
+				<li>
+					Het is succesvol geïnstalleerd!
+				</li>
+			</ol>
+
+		</div>
+
+	</div>
+
+	<?php
+	include('../../../components/footerChapter.php');
+	?>
+
+</body>
---
+++ bestanden/pages/theorie/H7/installerenTrello.php
@@ -0,0 +1,46 @@
+<?php
+	include('../../../components/headerChapter.php');
+?>
+
+<body>
+
+	<div class="title-small">
+		<h2>
+			H7 Trello installeren
+		</h2>
+	</div>
+
+	<div class="installation-bar">
+		<h3>Trello</h3>
+	</div>
+
+	<div class="theorie">
+		<div class="bar-s">
+			<h3>
+				Installatie instructies
+			</h3>
+		</div>
+
+		<div class="theorie-content">
+			<pre>
+Trello is op meerdere manieren te gebruiken, je kunt de webversie gebruiken, het installeren op een desktop of op een telefoon.
+
+Voor de webversie hoef je alleen maar naar <a href="https://trello.com">https://trello.com</a> te gaan.
+
+Met windows 8 of 10 kun je trello eenvoudig installeren via de microsoft store. Je opent de store en zoekt op trello (van Trello, inc.) en klikt op install.
+
+Er is ook een variant die te krijgen is via de App store voor zowel mobiel als desktop.
+Voor android apparaten is de app in de playstore te vinden onder de naam Trello (van Trello, inc.).
+
+Er is geen officiële (non apple/android) linux variant te krijgen of een versie voor oudere windows apparaten.
+			</pre>
+
+		</div>
+
+	</div>
+
+	<?php
+	include('../../../components/footerChapter.php');
+	?>
+
+</body>

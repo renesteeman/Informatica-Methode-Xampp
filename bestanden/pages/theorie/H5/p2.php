ca689224d3c14a0f5a0f79331b0d168b13e84482
Main <steeman.rene1@gmail.com>
2018-05-22 18:21:15 +0200

    quiz H5

--- bestanden/pages/theorie/H5/p2.php
+++ bestanden/pages/theorie/H5/p2.php
@@ -67,7 +67,6 @@ Zoals eerder genoemd bestaat een webpagina vaak uit 3 belangrijke onderdelen: de
 &lt;/html>
 </code>
 
-
 Het valt misschien op dat er ook &lt;head> bij staat, hierin zet je normaal algemene dingen die de pagina nodig heeft om te werken, al wordt dit tegenwoordig ook vaak in de &lt;footer> gezet zodat eerst de inhoud van de pagina geladen wordt en daarna pas de rest, dan lijkt de pagina sneller te zijn. Zo kan er verwezen worden naar een css bestand of een andere code.
 Je ziet ook dat onderdelen beginnen met <> en eindigen met </>, dit komt bij bijna elk element (onderdeel) in HTML voor. Tussen deze ‘blokken’ kun je de inhoud zetten.
 Een pagina met als inhoud “Hello HTML” zou er als volgt uit kunnen zien.
@@ -89,13 +88,9 @@ Hello HTML
 &lt;/html>
 </code>
 
-
 Stel je wilt een hele paragraaf als tekst, dan kun je &lt;p> gebruiken. De p staat voor paragraph. Of om een ‘blok’ te detineren kun je &lt;div> gebruiken, dit staat voor divisie. Dit kun je later met css bijvoorbeeld een achtergrondkleur geven.
 Als je HTML gaat schrijven kan dit in word, maar het is beter om een gespecialiseerd programma voor te gebruiken. Zelf (de maker van deze website) gebruik ik atom. Dit is een gratis tekstbewerker waar je zelf veel aan kunt aanpassen en toevoegen. Je kunt het vinden op https://atom.io/ . Je kunt een bestand dan opslaan met de extensie html (dus je eindigt de naam van het bestand met .html). Dit kun je dan openen met de browser. Ik adviseer om als browser geen gebruik te maken van internet explorer, aangezien dit slechte ondersteuning heeft.
-
 			</pre>
-
-
 		</div>
 
 		<div class="bar-s">

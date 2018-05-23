eccf5ebb47c2372907dff9ce76a86526da1faf5a
Main <steeman.rene1@gmail.com>
2018-05-23 10:30:42 +0200

    quiz H1

--- bestanden/pages/theorie/H1/quiz.php
+++ bestanden/pages/theorie/H1/quiz.php
@@ -19,30 +19,30 @@
 	<div class="vragen">
 		<!-- vraag 1-->
 		<div class="vraagBalk">
-			Wat is 1000111 binair in het decimaal systeem?
+			Wat is 100111 binair in het decimaal systeem?
 		</div>
 		<div class="antwoorden">
 			<ul>
 				<li>
-					<label class="container">113
-						<input type="checkbox" class="single-select-checkbox">
-						<span class="checkmark"></span>
-					</label>
-				</li>
-				<li>
-					<label class="container">80
-						<input type="checkbox" class="single-select-checkbox">
-						<span class="checkmark"></span>
-					</label>
-				</li>
-				<li>
-					<label class="container">71
-						<input type="checkbox" class="single-select-checkbox">
-						<span class="checkmark"></span>
-					</label>
-				</li>
-				<li>
 					<label class="container">4
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">103
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">39
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">7
 						<input type="checkbox" class="single-select-checkbox">
 						<span class="checkmark"></span>
 					</label>
@@ -55,30 +55,156 @@
 
 		<!-- vraag 2-->
 		<div class="vraagBalk">
-			ahdsf
+			Wat is 1001 * 0110 in binair?
 		</div>
 		<div class="antwoorden">
 			<ul>
 				<li>
-					<label class="container">113
+					<label class="container">15
 						<input type="checkbox" class="single-select-checkbox">
 						<span class="checkmark"></span>
 					</label>
 				</li>
 				<li>
-					<label class="container">80
-						<input type="checkbox" class="single-select-checkbox">
-						<span class="checkmark"></span>
-					</label>
-				</li>
-				<li>
-					<label class="container">71
+					<label class="container">18
 						<input type="checkbox" class="single-select-checkbox">
 						<span class="checkmark"></span>
 					</label>
 				</li>
 				<li>
 					<label class="container">4
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">54
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+			</ul>
+		</div>
+		<div class="uitleg">
+
+		</div>
+
+		<!-- vraag 3-->
+		<div class="vraagBalk">
+			Wat is geen voordeel van een GPU over een CPU?
+		</div>
+		<div class="antwoorden">
+			<ul>
+				<li>
+					<label class="container">De GPU kan meer taken tegelijk uitvoeren
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">De GPU kan complexere berekeningen uitvoeren
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">De GPU heeft meer opslag
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+			</ul>
+		</div>
+		<div class="uitleg">
+
+		</div>
+
+		<!-- vraag 4-->
+		<div class="vraagBalk">
+			Wat is geen voordeel van een (standard) SDD over een (standard) HDD?
+		</div>
+		<div class="antwoorden">
+			<ul>
+				<li>
+					<label class="container">Het formaat (fysiek)
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">De snelheid
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">De prijs
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">Betrouwbaarheid
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+			</ul>
+		</div>
+		<div class="uitleg">
+
+		</div>
+
+		<!-- vraag 5-->
+		<div class="vraagBalk">
+			Wat is geen (kern)taak van het OS?
+		</div>
+		<div class="antwoorden">
+			<ul>
+				<li>
+					<label class="container">Het registreren van input
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">Het beheren van RAM
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">Het vertalen van code naar machinetaal
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">Het beheren van processen
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+			</ul>
+		</div>
+		<div class="uitleg">
+
+		</div>
+
+		<!-- vraag 6-->
+		<div class="vraagBalk">
+			Wat is de uitkomst als bij een OR-gate 1, 1 wordt uitgevoerd en het resultaat hiervan naar de bovenkant van een AND-gate gaat waar aan de onderkant 1 binnenkomt?
+		</div>
+		<div class="antwoorden">
+			<ul>
+				<li>
+					<label class="container">1
+						<input type="checkbox" class="single-select-checkbox">
+						<span class="checkmark"></span>
+					</label>
+				</li>
+				<li>
+					<label class="container">0
 						<input type="checkbox" class="single-select-checkbox">
 						<span class="checkmark"></span>
 					</label>

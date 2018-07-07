d8645df233f2e95d0bed0d1137e5549072ecb9ab
René Steeman <steeman.rene1@gmail.com>
2018-07-07 09:15:20 +0200

    H1.1 updated

--- bestanden/pages/theorie/H1/p1.php
+++ bestanden/pages/theorie/H1/p1.php
@@ -50,50 +50,37 @@
 
 		<div class="theorie-content">
 			<p>
-				<p>
-					Een computer ‘begrijpt’ alleen 0’en en 1’en. informatie word in een computer altijd doormiddel van deze twee getallen opgeslagen, 'taken' die heen en weer worden gestuurd door de compter bestaan alleen uit 1' en en 0'
+				Een computer ‘begrijpt’ alleen 0’en en 1’en. Informatie wordt in een computer altijd doormiddel van deze twee getallen opgeslagen, ook de opdrachten die heen en weer worden verzonden bestaan uit dezen 1'en en 0'en.
+			</p>
+			<p>
+				Een bit heeft twee mogelijke waardes: 1 en 0. Een 1 geeft de aanwezigheid en 0 de afwezigheid van stroom aan. Er kan met dit binair systeem vanalles worden weergegeven. Het werkt het beste als er meerden 0'en en 1'en elkaar opvolgen om te zorgen voor veel mogelijkheden. Hiermee wordt alles op een computer uitgevoerd.
+			</p>
+			<p>
+				In het binair stelsel wordt van rechts naar links geteld. De waarde van een 1 wordt steeds groter als het verder links staat. Als voorbeeld kun je zien hoe je in het binair stelsel tot 10 telt (met stappen van 1).
 
-				</p>
-				0' en en 1' en worden begrepen door de af- of aanwezigheid van stroom, een 0 voor de afwezigheid van stroom en een 1 voor de aanwezigheid van stroom
-				<p>
-				een bit heeft twee mogelijke waardes: 1 en 0. zoals we al aangegaven is een 1 de aanwezigheid en 0 de afwezigheid van stroom, met deze afwezigheid en de aanwezigheid kun je een heleboel aanduiden: aan (1) of uit (0), geslacht: (0) vrouw (1) man en meer.
-				</p>
-
-				<p>
-					In binair stelsel wordt van rechts naar links geteld net zoals bij het decimaal stelsel dat wij gebruiken. alleen is hierbij niet 9 de grenswaarde maar 1:
-				</p>
-
-				<p>
-				naar het getal 10 tellen in het decimaal stelsel: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-				</p>
-
-				<p>
-					naar het getal 10 tellen in het binar stelsel:0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010
-				</P>
+				0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010
 			</p>
 
 			<p>
-				alle 1'en samen, inclusief de nullen ertussen, geven een volledige waarde weer. de waarde van de 1 wordt bepaald door de plek die de 1 heeft binnen de reeks:
+				Alle 1'en en 0'en geven een volledige waarde weer. De waarde van de 1 wordt bepaald door de plek die de 1 heeft binnen de reeks. Helemaal rechts is het 1 waard en voor elke plek dat het verder naar links staat verdubbeld zijn waarde.
+				Een paar voorbeelden zijn:
 			</p>
 
 			<p>
-				een 1 op de tweede plek is 2 waard, dus 10 in binair stelsel is 2
-				</p>
+				Een 1 op de tweede plek van rechts is 2 waard, dus 10 in binair stelsel is 2 in het decimaal systeem.
+			</p>
 
-				<p>
-					een 1 op de derde plek is 4 waard, dus een 100 in het binair stelsel is 4
-				</p>
+			<p>
+				Een 1 op de derde plek is 4 waard, dus een 100 in het binair stelsel is 4.
+			</p>
 
-				<p>
-					een 1 op de vierde plek is 8 waard, dus een 1000 in het binar stelsel is 8
-				</p>
+			<p>
+				Een 1 op de vierde plek is 8 waard, dus een 1000 in het binar stelsel is 8.
+			</p>
 
-				<p>
-					een 1 op de vijfde plek is 16 waar, en zo verdubbelt zich het steeds
-				</p>
-
-				<p>
-					zo zie je dat je het getal 0 of 1 in een bit kan uitdrukken, maar dat je 2 bits nodig hebt om het getal 2 in het binar stelsel uit te drukken, want na 0 en 1 komt 10.
+			<p>
+				Je kunt ook een 0 eraan toevoegen, dit hebt je bijvoorbeeld nodig om 5 weer te geven in decimaal, 101. Een 0 kun dus een waarde van een 1 verhogen, door de 1 naar links te 'duwen'.
+			</p>
 
 		</div>
 

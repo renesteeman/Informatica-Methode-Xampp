4373c7ef0482e388c086a2519b09ee0abd11e5fd
René Steeman <steeman.rene1@gmail.com>
2018-07-09 21:01:27 +0200

    GUI part2

--- bestanden/pageCreator/CreateParagraphs.py
+++ bestanden/pageCreator/CreateParagraphs.py
@@ -1,12 +1,21 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 
 def contiueToParagraphs(*args):
     try:
-        value = int(Nparagraphs_entry.get())
+        CapterName = ChapterName_entry.get()
+        Nparagraphs = int(Nparagraphs_entry.get())
     except ValueError:
         pass
 
+def openFileSelector(*args):
+    try:
+        root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt *.pdf *.docx"),("all files","*.*")))
+    except ValueError:
+        pass
+
+ChapterName = StringVar
 Nparagraphs = StringVar
 
 root = Tk()
@@ -17,16 +26,26 @@ mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
+#algemene info over het hoofdstuk
+
+ttk.Label(mainframe, text="hoofdstuk naam: ").grid(column=1, row=1, sticky=W)
+ChapterName_entry = ttk.Entry(mainframe, width=7, textvariable=ChapterName)
+ChapterName_entry.grid(column=2, row=1, sticky=(W,E))
+
+ttk.Label(mainframe, text="aantal paragrafen: ").grid(column=1, row=2, sticky=W)
 Nparagraphs_entry = ttk.Entry(mainframe, width=7, textvariable=Nparagraphs)
-Nparagraphs_entry.grid(column=2, row=1, sticky=(W,E))
+Nparagraphs_entry.grid(column=2, row=2, sticky=(W,E))
 
-ttk.Button(mainframe, text="contiue", command=contiueToParagraphs).grid(column=1, row=2, sticky=W)
+#bestanden selecteren
+ttk.Label(mainframe, text="selecteer het te laden bestand").grid(column=1, row=3, sticky=W)
+ttk.Button(mainframe, text="selecteer bestand", command=openFileSelector).grid(column=2, row=3, sticky=W)
 
-ttk.Label(mainframe, text="aantal paragraven: ").grid(column=1, row=1, sticky=W)
+#doorgaan naar volgende pagina
+ttk.Button(mainframe, text="volgende", command=contiueToParagraphs).grid(column=1, row=4, sticky=W)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-Nparagraphs_entry.focus()
+ChapterName_entry.focus()
 root.bind('<Return>', contiueToParagraphs)
 
 root.mainloop()

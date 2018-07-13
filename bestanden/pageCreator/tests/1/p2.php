3b886b2b68119cc162d0b25dafd969390a024da1
René Steeman <steeman.rene1@gmail.com>
2018-07-13 09:23:54 +0200

    <p> working (at least on .txt)

---
+++ bestanden/pageCreator/tests/1/p2.php
@@ -0,0 +1,40 @@
+
+<?php
+include('../../../components/headerChapter.php');
+?>
+
+<body>
+
+    <div class="title-small">
+        <h2> 
+        1 
+        </h2>
+    </div>
+
+    <div class="bar-par-overview">
+        <div class="paragraph-tiles">
+    
+    <div class="ptile">
+        <span class="ptile-content"><a href="p1.php">
+        §1
+        </a></span>
+    </div>
+            
+    <div class="ptile active">
+        <span class="ptile-content"><a href="p2.php">
+        §2
+        </a></span>
+    </div>
+            </div>
+    </div>
+
+    <div class="theorie">
+        <div class="bar-s">
+            <h3>
+                Theorie
+            </h3>
+        </div>
+
+        <div class="theorie-content">
+            
+    

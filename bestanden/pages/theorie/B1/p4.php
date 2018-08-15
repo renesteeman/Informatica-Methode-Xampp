781d4ba35309eb8ddb21faf47238be435432549b
René Steeman <steeman.rene1@gmail.com>
2018-08-15 09:18:16 +0200

    B1 p4

--- bestanden/pages/theorie/B1/p4.php
+++ bestanden/pages/theorie/B1/p4.php
@@ -5,8 +5,8 @@ include('../../../components/headerChapter.php');
 <body>
 
 	<div class="title-small">
-		<h2> 
-			B1 §4 
+		<h2>
+			B1 §4 Kunstmatige intelligentie
 		</h2>
 	 </div>
 
@@ -48,22 +48,22 @@ include('../../../components/headerChapter.php');
 		</div>
 
 		<div class="theorie-content">
-        
+
 			<p>Kunstmatige intelligentie</p>
 
-			<p>Kunstmatige/artificiële intelligentie is een wetenschappelijke veld waarin het creëren en gebruik van AI wordt besproken. Je hebt vast wel in films zoals The Matrix en Terminator gehoord van de Singularity die de mensheid wilt vernietigen, hoe heeft dit onderwerp met informatica en jouw te maken?</p>
+			<p>Kunstmatige intelligentie is een wetenschappelijke veld waarin het creëren en gebruik van AI wordt besproken. Je hebt vast wel in films zoals The Matrix en Terminator gehoord, maar hoe heeft dit onderwerp met informatica en jouw te maken?</p>
 
 			<p>Een AI wordt vaak verdeeld tussen twee soorten, “strong” AI en “weak” AI:</p>
 
 			<p>Weak AI</p>
 
-			<p>Weak AI bestaat al in onze maatschappij, een Tesla auto berekent wat het ziet en probeert niet de auto te crashen, het programma is gemaakt om de de auto te besturen en het kan niks anders. Dit is gebaseerd op mechanica zoals machine learning. Een weak AI is dus een AI met maar een doel. Het verschil tussen een weak AI en een mens is dat de AI nooit zal denken aan de consequenties. Wanneer je de opdracht geeft aan een weak AI om zo veel mogelijk ijsjes te maken kan het alle mensen vermoorden, veranderen naar een pasta en zo vlees ijsjes maken.</p>
+			<p>Weak AI bestaat al in onze maatschappij, een Tesla auto berekent wat het ziet en probeert niet ergens tegen te botsen, het programma is gemaakt om de de auto te besturen en het kan niks anders. Dit is gebaseerd op technologieën zoals machine learning. Een weak AI is dus een AI met maar één doel. Wanneer je de opdracht geeft aan een weak AI om zo veel mogelijk ijsjes te maken kan het alle mensen vermoorden, veranderen naar een pasta en zo vlees ijsjes maken. Het heeft dus wel duidelijke regels nodig! Deze regels worden verwerkt in een score systeem, des te beter het resultaat, des te meer punten. Een AI kan hiermee getraind worden en zich zo 'goed gaan gedragen'.</p>
 
-			<p>In onze samenleving wordt weak AI toegepast op verschillende industrieën, omdat een weak AI sneller en efficiënter is dan een mens kan het gemakkelijk simpele taken en banen vervangen. Dit heeft gevolgen voor allerlei industrieën, de mensen die hun banen verliezen zijn vaak niet in staat om betere opleidingen te zoeken. Het kan resulteren in massale werkloosheid en economische crisis.</p>
+			<p>In onze samenleving wordt weak AI toegepast op verschillende industrieën, omdat een weak AI sneller en efficiënter is dan een mens, kan het gemakkelijk simpele taken en banen vervangen. Dit heeft gevolgen voor allerlei industrieën, de mensen die hun banen verliezen zijn vaak niet in staat om betere opleidingen te zoeken. Het kan resulteren in massale werkloosheid en daardoor economische crisis. Het kan er ook voor zorgen dat de mens niet meer of minder hoeft te werken en meer vakantie heeft.</p>
 
 			<p>Strong AI</p>
 
-			<p>Strong AI bestaat nog niet voor ons, maar in theorie zal het een programma zijn dat de volledig het diepte van menselijk begrip en logica kunt volgen, waaronder dus ook emoties. Het verschil tussen deze vorm van AI en een mens is dat de AI veranderingen kan brengen in zijn programmering. Een mens doet miljoenen jaren erover om zijn brein significant te verbeteren, een computer kan makkelijk een nieuw gpu toevoegen en nog sneller denken.</p>
+			<p>Strong AI bestaat nog niet voor ons, maar in theorie zal het een programma zijn dat de volledig het diepte van menselijk begrip en logica kan volgen, waaronder dus ook emoties. Het verschil tussen deze vorm van AI en een mens is dat de AI veranderingen kan aanbrengen in zijn code. Een mens doet miljoenen jaren erover om zijn brein significant te verbeteren, een computer kan makkelijk een nieuw cpu toevoegen en nog sneller denken.</p>
 
 		</div>
 
@@ -75,19 +75,23 @@ include('../../../components/headerChapter.php');
 
 		<div class="theorie-content">
 			<ol>
-				<li>Vragen:
+				Dit is natuurlijk een generalisatie van dit onderwerp en er kan veel dieper op worden ingegaan. Zo kunnen er meerdere vragen worden gesteld hierover.
 
-Dit is natuurlijk een generalisatie van dit onderwerp en er kan veel dieper op worden ingegaan. Zo kunnen er meerdere vragen worden gesteld hierover.
+				Deze vragen hebben geen concrete antwoorden, ze zijn reflecties van wie je bent en hoe je denkt. De antwoorden hieronder zijn dus maar voorbeelden. Probeer het standpunt van jouw antwoord te nemen en het te discussiëren met iemand met een verschillend standpunt.</br></br>
 
-Deze vragen hebben geen concrete antwoorden, ze zijn reflecties van wie je bent en hoe je denkt. Probeer het standpunt van jouw antwoord te nemen en het te discussiëren met iemand met een verschillend standpunt. 
+				<li>
+					Wat is een utopia die zich kan voldoen door AI?
+				</li>
+				<li>
+					Wat is een dystopia die zich kan voldoen door AI?
+				</li>
+				<li>
+					Hoe moet je AI controleren?
+				</li>
+				<li>
+					Vanaf wanneer is AI gelijkwaardig aan een mens?
+				</li>
 
-Wat is een utopia  die zich kan voldoen met AI?
-
-Wat is een dystopia die zich kan voldoen met AI?
-
-Hoe moet je AI controleren?
-
-Vanaf wanneer is AI een mens?</li>
 			</ol>
 		</div>
 
@@ -99,39 +103,33 @@ Vanaf wanneer is AI een mens?</li>
 
 		<div class="theorie-content theorie-answers">
 			<ol>
-				<li>Antwoord vraag 1:
+				<li>
+					In een wereld waarin de mensheid succesvol AI leert te controleren zal AI wetenschappelijke velden zoals natuurkunde en wiskunde ondersteunen of zelfs overnemen, hierdoor zal de kwaliteit van menselijk leven omhoog gaan.
+				</li>
+				<li>
+					In een wereld waarin de mensheid AI heeft gemaakt zonder het te kunnen controleren zal de AI de mensheid kunnen manipuleren tot het punt waarbij we allemaal moeten buigen aan een AI overlord. Niet een onbekend probleem.
+				</li>
+				<li>
+					Dus, de sleutel tot een AI utopia is het controleren van de AI. Hoe kunnen we dit doen? Volgens de populaire science fiction schrijver “Isaac Asimov” kan je door middel van drie wetten een AI controleren.
 
-In een wereld waarin de mensheid succesvol AI leert te controleren zal AI wetenschappelijke velden zoals natuurkunde en wiskunde ondersteunen of zelfs overnemen, hierdoor zal de kwaliteit van menselijk leven omhoog gaan.
+					Een robot mag een mens geen letsel toebrengen, noch, door passief te blijven, een mens letsel laten overkomen.
 
-Antwoord vraag 2:
+					Een robot moet de door mensen gegeven orders gehoorzamen behalve wanneer die orders in strijd zijn met de Eerste Wet.
 
-In een wereld waarin de mensheid AI heeft gemaakt zonder het te kunnen controleren zal de AI de mensheid kunnen manipuleren tot het punt waarbij we allemaal moeten buigen aan een AI overlord. Niet een onbekend probleem.
+					Een robot moet zichzelf beschermen zolang of voor zover dat niet met de Eerste of Tweede Wet in strijd is.
 
-Antwoord vraag 3:
+					Hoewel deze wetten de grootste problemen oplossen, heeft het toch loopholes. Zo kan een AI een de politiek in gaan, het kan nog steeds mensen besturen zonder dat de drie wetten worden verbroken.
+				</li>
+				<li>
+					In het geval van strong AI kan een robot zelfbewustzijn claimen, maar waar kunnen we de lijn van zelfbewustzijn neerzetten?
 
-Dus, de sleutel tot een AI utopia is het controleren van de AI. Hoe kunnen we dit doen? Volgens de populaire science fiction schrijver “Isaac Asimov” kan je door middel van drie wetten een AI controleren.
+					In het gedachte-experiment van Frank Jackson wordt de volgende situatie beschreven.
 
-Een robot mag een mens geen letsel toebrengen, noch, door passief te blijven, een mens letsel laten overkomen.
+					Mary is meisje dat is opgegroeid in een zwart en witte kamer. Er is een TV in zwart-wit. Mary kan zichzelf niet zien. Ze besteedt haar hele leven om kleuren te bestuderen, wat is blauw, wat is rood etc. Ze ontdekt de golflengtes dat de menselijke oog kan innemen en hoe de brein dit opneemt. Op haar 18e verjaardag krijgt ze als cadeau een rode appel, leert Mary nu iets nieuws?
 
-Een robot moet de door mensen gegeven orders gehoorzamen behalve wanneer die orders in strijd zijn met de Eerste Wet.
+					De vraag achter dit gedachte-experiment is of er een verschil is tussen het leren van iets en het ervaren van iets. Dit is ook appliceerbaar op het idee van bewustzijn. Als een strong AI (we noemen hem Karel) alles leert over het brein en het brein kan simuleren, heeft Karel dan bewustzijn bereikt? En zo ja, is hij dan gelijkwaardig aan een mens?
+				</li>
 
-Een robot moet zichzelf beschermen zolang of voor zover dat niet met de Eerste of Tweede Wet in strijd is.
-
-Hoewel deze wetten de grootste problemen oplossen, heeft het toch loopholes. Zo kan een AI een de politiek in gaan, het kan nog steeds mensen besturen zonder dat de drie wetten worden verbroken.
-
-Antwoord vraag 4:
-
-In het geval van strong AI kan een robot zelfbewustzijn claimen, maar waar kunnen we de lijn van zelfbewustzijn neerzetten?
-
-In het gedachte-experiment van Frank Jackson wordt de volgende situatie beschreven.
-
-
-
-Mary is meisje dat is opgegroeid in een zwart en witte kamer. Er is een TV in zwart-wit. Mary kan zichzelf niet zien. Ze besteedt haar hele leven om kleuren te bestuderen, wat is blauw, wat is rood etc. Ze ontdekt de golflengtes dat de menselijke oog kan innemen en hoe de brein dit opneemt. Op haar 18e verjaardag krijgt ze als cadeau een rode appel, leert Mary nu iets nieuws?
-
-
-
-De vraag achter dit gedachte-experiment is of er een verschil is tussen het leren van iets en het ervaren van iets. Dit is ook appliceerbaar op het idee van bewustzijn. Als een strong AI (we noemen hem Karel) alles leert over het brein en het brein kan simuleren, heeft Karel dan bewustzijn bereikt?</li>
 			</ol>
 		</div>
 	</div>

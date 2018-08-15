0cd7a1dbf6c925794a537a7ae3f21b6a62d564a5
René Steeman <steeman.rene1@gmail.com>
2018-08-15 08:55:26 +0200

    B1 p3

--- bestanden/pages/theorie/B1/p3.php
+++ bestanden/pages/theorie/B1/p3.php
@@ -5,8 +5,8 @@ include('../../../components/headerChapter.php');
 <body>
 
 	<div class="title-small">
-		<h2> 
-			B1 §3 
+		<h2>
+			B1 §3 Het trein probleem
 		</h2>
 	 </div>
 
@@ -48,38 +48,92 @@ include('../../../components/headerChapter.php');
 		</div>
 
 		<div class="theorie-content">
-        
+
 			<p>Nieuwe filosofen hebben vast iets hierover gehoord. Stel je voor:</p>
 
 			<p>Je zit in een treinwagon, voor je gaat de spoor op in twee kanten, op spoor A staat een mens, op spoor B staan vijf mensen. Je kunt de trein niet stoppen. Wat doe je?</p>
 
-			<p>Je volgt spoor A.</p>
-
-			<p>Je volgt spoor B.</p>
-
-			<p>Je maakt geen keuze.</p>
+			<ol>
+				<li>
+					Je volgt spoor A.
+				</li>
+				<li>
+					Je volgt spoor B.
+				</li>
+				<li>
+					Je maakt geen keuze.
+				</li>
+			</ol>
 
 			<p>Voor de meeste mensen is dit een makkelijke keuze, wanneer je het leven waardeert zal je willen dat de minste hoeveelheid mensen dood gaan. Volgens het gevolgenethiek van Bentham en Mill zou je dus spoor A volgen en de vijf mensen redden.</p>
 
 			<p>Er zijn meerdere varianten van dit denk-experiment, wat nou als de mensen op de sporen veranderen? Wat zou je kiezen wanneer:</p>
 
-			<p>A: vijf mensen,                           B: zes mensen.</p>
-
-			<p>A: een jong kind,                        B: 5 gepensioneerde leraren..</p>
-
-			<p>A: Koning Willem-Alexander,     B: Een geschiedenis student.</p>
-
-			<p>A: Een natuurkunde professor,  B: Een ICT professor.</p>
-
-			<p>A: Jouw ouders,                         B: 30 onschuldige mensen.</p>
+			<ol class="MLquestion">
+				<li>
+					<ol>
+						<li>
+							vijf mensen
+						</li>
+						<li>
+							zes mensen
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							een jong kind
+						</li>
+						<li>
+							5 gepensioneerde leraren
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							Koning Willem-Alexander
+						</li>
+						<li>
+							Een geschiedenis student
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							Een natuurkunde professor
+						</li>
+						<li>
+							Een ICT professor
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							Jouw ouders
+						</li>
+						<li>
+							30 onschuldige mensen
+						</li>
+					</ol>
+				</li>
+			</ol>
 
 			<p>Zoals je waarschijnlijk al hebt gemerkt zijn sommige keuzes makkelijker dan de andere. De vraag is natuurlijk wat dit te maken heeft met programmeren.</p>
 
 			<p>Stel, Elon Musk is een grote fan van jouw programma's en neemt jou aan als een developer voor de zelfrijdende Tesla auto’s. Hoewel het doel van een Tesla is om menselijke fouten te vermijden zijn sommige fouten onvermijdelijk. Als een Tesla rond aan het rijden is met een familie die net terugkomt van Disneyland, twee straten voordat ze thuiskomen steekt een klas van groep vier kinderen over. De Tesla kan nu of:</p>
 
-			<p>A: Tegen een boom aan botsen, riskeer de familie en red de kinderen of</p>
-
-			<p>B: Probeer uit te wijken, riskeer de kinderen aan te botsen.</p>
+			<ol>
+				<li>
+					Tegen een boom aan botsen, riskeer de familie en red de kinderen.
+				</li>
+				<li>
+					Probeer uit te wijken, riskeer de kinderen aan te botsen.
+				</li>
+			</ol>
 
 			<p>Een computer is niet in staat om dit probleem op te lossen. Wat moet jij als programmeur hier aan doen? Als mens heb je een set normen en waarden, regels wat je zijn aangeleerd sinds dat je bent geboren. Je mag bijvoorbeeld niet zomaar stelen of liegen. Een algoritme heeft dat niet. Wanneer een computer moet bepalen wie het moet aanrijden denkt het niet aan interpersoonlijke relaties, het denkt niet aan de waardes dat mensen nog hebben in de samenleving, dat is jouw taak.</p>
 
@@ -93,29 +147,62 @@ include('../../../components/headerChapter.php');
 
 			<p>Maar, als we mensen zouden prioriteren gebaseerd op hun nuttigheid in de maatschappij, moeten we dan de werklozen en drugsverslaafde ook minder meetellen? Het volgt dezelfde regel, dus:</p>
 
-			<p>-Mensen krijgen een lagere telling gebaseerd op hun contributie in de maatschappij.</p>
+			<p>-Mensen krijgen een andere telling gebaseerd op hun contributie in de maatschappij.</p>
 
-			<p>Zo kan je natuurlijk doorgaan, begin met de nutteloze mensen, ga door met de ladder totdat je een hele regelboek hebt bedacht voor jouw Tesla. Nu is het tijd om het in de praktijk uit te testen. Laten we hiervoor de A&B vragen nemen die hiervoor zijn gesteld. We gaan hierbij vanuit dat de kant dat de Tesla besluit om over te rijden absoluut dood tegengaat.</p>
+			<p>Zo kan je natuurlijk doorgaan, begin met het aantal mensen en ga door totdat je een hele regelboek hebt bedacht voor jouw Tesla. Nu is het tijd om het in de praktijk uit te testen. Laten we hiervoor de A&B vragen nemen die hiervoor zijn gesteld. Wat zou de Tesla volgens jou moeten kiezen voor het beste resultaat?</p>
 
-			<p>A: vijf mensen,                           B: zes mensen.</p>
-
-			<p>A: een jong kind,                        B: 5 gepensioneerde werkers.</p>
-
-			<p>A: Koning Willem-Alexander,     B: Een geschiedenis student.</p>
-
-			<p>A: Een natuurkunde professor,  B: Een ICT professor.</p>
-
-			<p>A: Jouw ouders,                         B: 30 onschuldige mensen.</p>
-
-			<p>A, simpele volging van de eerste regel, probeer zo weinig mogelijk mensen te doden.</p>
-
-			<p>B, hoewel je bij deze keuze meer mensen dood zijn de mensen niet meer nuttig voor de samenleving, het kind heeft een kans om de volgende Einstein worden.</p>
-
-			<p>?, hoe nuttig is de koning is een constitutioneel monarchie? Hoe nuttig is een leerling geschiedenis?</p>
-
-			<p>?, wat is een belangrijkere vak, natuurkunde of informatica? Wat is belangrijker voor de toekomst van de mens?</p>
-
-			<p>B, jouw ouders kunnen wel belangrijk zijn, maar de kans voor nuttige mensen in de dertig onschuldige mensen is veel hoger en daarom is het beter om hun leven te redden?</p>
+			<ol class="MLquestion">
+				<li>
+					<ol>
+						<li>
+							vijf mensen
+						</li>
+						<li>
+							zes mensen
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							een jong kind
+						</li>
+						<li>
+							5 gepensioneerde leraren
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							Koning Willem-Alexander
+						</li>
+						<li>
+							Een geschiedenis student
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							Een natuurkunde professor
+						</li>
+						<li>
+							Een ICT professor
+						</li>
+					</ol>
+				</li>
+				<li>
+					<ol>
+						<li>
+							Jouw ouders
+						</li>
+						<li>
+							30 onschuldige mensen
+						</li>
+					</ol>
+				</li>
+			</ol>
 
 			<p>Misschien heb je het al opgemerkt, maar nu ben je niks anders aan het doen dan het opstellen van waarden aan mensen gebaseerd op verschillende eigenschappen.</p>
 

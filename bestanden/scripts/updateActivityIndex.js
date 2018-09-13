1092bbc47660bb77f9e8d5da354e259e27eb2ab7
René Steeman <steeman.rene1@gmail.com>
2018-09-13 16:22:31 +0200

    Only update activity when the page is actually active

--- bestanden/scripts/updateActivityIndex.js
+++ bestanden/scripts/updateActivityIndex.js
@@ -1,23 +1,35 @@
 $(document).ready(function(){
 
-  function executeAjax(){
-    jqXHR = $.ajax({
-  		method: "POST",
-  		url: 'scripts/updateActivity.php'
-  	})
+  function updateActivity(){
 
-    jqXHR.done(function(msg) {
-  		if(msg.length > 0){
-        alert(msg);
-      }
-  	})
+    window.onblur = function(){
+			active = 0;
+		}
 
-  	jqXHR.fail(function(jqXHR) {
-  	  alert("Er is iets mis gegaan met AJAX, de foutcode is " + jqXHR.status + " met als beschrijving " + jqXHR.statusText + ". Neem alstublieft contact op met koffieandcode@gmail.com en noem zowel de pagina als de inhoud van dit bericht. Alvast erg bedankt!");
-  	})
+		window.onfocus = function(){
+			active = 1;
+		}
 
-    setTimeout(executeAjax, 60000);
+    if(active){
+      jqXHR = $.ajax({
+    		method: "POST",
+    		url: 'scripts/updateActivity.php'
+    	})
+
+      jqXHR.done(function(msg) {
+    		if(msg.length > 0){
+          alert(msg);
+        }
+    	})
+
+    	jqXHR.fail(function(jqXHR) {
+    	  alert("Er is iets mis gegaan met AJAX, de foutcode is " + jqXHR.status + " met als beschrijving " + jqXHR.statusText + ". Neem alstublieft contact op met koffieandcode@gmail.com en noem zowel de pagina als de inhoud van dit bericht. Alvast erg bedankt!");
+    	})
+    }
+
+    setTimeout(updateActivity, 60000);
   }
 
-  executeAjax();
+  active = 1;
+  updateActivity();
 })

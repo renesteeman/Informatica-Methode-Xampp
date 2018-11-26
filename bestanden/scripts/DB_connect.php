c584e2626b8449566ec6205adfd239b8e626a960
René Steeman <steeman.rene1@gmail.com>
2018-11-26 15:52:20 +0100

    modern main part 1

--- bestanden/scripts/DB_connect.php
+++ bestanden/scripts/DB_connect.php
@@ -1,21 +1,21 @@
 <?php
 	//prepare for DB connection
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'inforca');
+  define('DB_SERVER', 'localhost');
+  define('DB_USERNAME', 'root');
+  define('DB_PASSWORD', '');
+  define('DB_NAME', 'inforca');
 
 	//connect to DB
-    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+  $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 	//use utf8
 	mysqli_set_charset($conn, 'utf8mb4');
 
-    // Check connection
-    if($conn === false){
+  // Check connection
+  if($conn === false){
 
-      die("Kon geen verbinding maken met database");
+    die("Kon geen verbinding maken met database");
 
-    }
+  }
 
 ?>

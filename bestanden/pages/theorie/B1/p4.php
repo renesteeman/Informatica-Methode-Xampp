3674564a64c40b9375c26f3a59019d329ccedfdb
René Steeman <steeman.rene1@gmail.com>
2018-12-19 21:28:02 +0100

    AI paragraph extended and css fix

--- bestanden/pages/theorie/B1/p4.php
+++ bestanden/pages/theorie/B1/p4.php
@@ -48,9 +48,9 @@ include('../../../components/headerChapter.php');
 		</div>
 
 		<div class="theorie-content">
-			<p>Kunstmatige intelligentie is een wetenschappelijk veld waarin het creëren en gebruik van AI wordt besproken. Je hebt vast wel van films zoals The Matrix en Terminator gehoord, maar hoe heeft dit met informatica en met jouw te maken?</p>
+			<p>Kunstmatige intelligentie is een wetenschappelijk veld waarin het creëren en gebruik van AI wordt besproken. Je hebt vast wel van films zoals The Matrix en Terminator gehoord, maar hoe heeft dit met informatica en met jou te maken? In deze paragraaf ga je kennismaken met AI en de basis ervan leren. AI brengt veel filosofische met zich mee en is een snel veranderd veld.</p>
 
-			<p>AI wordt vaak verdeeld in twee soorten, “strong” AI en “weak” AI:</p>
+			<p>AI wordt vaak verdeeld in twee soorten, “strong” AI en “weak” AI.</p>
 
 			<p>Weak AI</p>
 
@@ -62,6 +62,19 @@ include('../../../components/headerChapter.php');
 
 			<p>Strong AI bestaat nog niet voor ons, maar in theorie zal het een programma zijn dat de volledig het diepte van menselijk begrip en logica kan volgen, waaronder dus ook emoties. Het verschil tussen deze vorm van AI en een mens is dat de AI veranderingen kan aanbrengen in zijn code. Een mens doet miljoenen jaren erover om zijn brein significant te verbeteren, een computer kan makkelijk een nieuw cpu toevoegen en nog sneller denken.</p>
 
+			<!--added recently TODO check-->
+			<p>
+				Een korte uitleg van de werking van AI
+			</p>
+
+			<p>
+				AI is een veld met daarin meerdere subvelden, zo valt machine learning er ook onder en onder machine learning valt deep learning. Machine learning wordt gebruikt om data te voorspellen door het kunnen maken van een formule dat zo accuraat mogelijk verschillende datapunten benaderd. Datapunten zijn waardes die de data die wordt ingevoerd weergeven. Deze ingevoerde data kan bijvoorbeeld een lijst van cijfers zijn van een leerling waarmee zijn volgende cijfer voorspelt kan worden. Deep learning gaat nog een stap verder, hierbij worden modellen (programma's) gemaakt die imiteren wat in de hersenen gebeurt bij het maken van keuzes. Zo'n model ook neural network genoemd begint met verschillende waardes die aangeven hoe zwaar een waarde vanuit de gegeven data meegeteld moet worden in een soort grote formule. Het model levert dan een waarde als resultaat, dit kan bijvoorbeeld een percentage zijn dat aangeeft hoe zeker het is dat een bepaald object getoond is of een getal dat aangeeft wat een student als cijfer gaat halen. Het gaat dan steeds meer data door en corrigeert zichzelf waardoor het steeds zijn gewichten, de waardes die bepalen hoeveel iets meetelt in de 'formule', kan verbeteren. Een voorbeeld van zo'n neural network is een programma dat 'geleerd' heeft te voorspellen of iets een kat of hond is. Een AI is dus een programma dat probeert een waarde te voorspellen, dit kan een getal zijn (bijvoorbeeld een cijfer voor een toets) of een categorie (bijvoorbeeld hond of kat).
+			</p>
+
+			<p>
+				Misschien denk je nu dat AI de oplossing is voor alle problemen, maar dat is het zeker niet. AI is goed voor het voorspellen van waardes, maar voor andere zaken zijn ze vaak niet nodig of handig. Het kost ook vaak veel moeite om een AI te ontwikkelen en het is niet altijd efficient om een AI te gebruiken. Je gaat bijvoorbeeld geen AI maken om een .pdf om te zetten naar een .docx. Het zou wel handig kunnen zijn voor het voorspellen of een student een studie gaat halen en misschien aan de hand daarvan bijles te regelen. Een AI is over het algemeen goed als snel en vaak een keuze of voorspelling gemaakt moet worden.
+			</p>
+
 		</div>
 
 		<div class="bar-s">
@@ -71,25 +84,38 @@ include('../../../components/headerChapter.php');
 		</div>
 
 		<div class="theorie-content">
-			<ol>
-				Dit is natuurlijk een generalisatie van dit onderwerp en er kan veel dieper op worden ingegaan. Zo kunnen er meerdere vragen worden gesteld hierover.
 
-				Deze vragen hebben geen concrete antwoorden, ze zijn reflecties van wie je bent en hoe je denkt. De antwoorden hieronder zijn dus maar voorbeelden. Probeer het standpunt van jouw antwoord te nemen en het te discussiëren met iemand met een verschillend standpunt.</br></br>
-
+			<ol class="MLquestionAlt2">
 				<li>
-					Wat is een utopia die zich kan voldoen door AI?
-				</li>
-				<li>
-					Wat is een dystopia die zich kan voldoen door AI?
-				</li>
-				<li>
-					Hoe moet je AI controleren?
-				</li>
-				<li>
-					Vanaf wanneer is AI gelijkwaardig aan een mens?
+					Geef bij de volgende problemen aan of deze geschikt zijn om opgelost te worden met AI.
+					<ol>
+						<li>Het automatisch dimmen van lichten aan de hand van de lichtintensiteit.</li>
+						<li>Het voorspellen hoeveel persoon veganist zullen zijn in 2030.</li>
+						<li>Voorspellen of een foto een kat of hond bevat voor 5000000 foto's.</li>
+						<li>Het bepalen van de locatie van een overvaller door de politie aan de hand van zijn geschiedenis.</li>
+					</ol>
 				</li>
 
+				<li>
+					De volgende vragen hebben geen concrete antwoorden, ze zijn reflecties van wie je bent en hoe je denkt. De antwoorden hieronder zijn dus maar voorbeelden. Probeer het standpunt van jouw antwoord te nemen en het te discussiëren met iemand met een verschillend standpunt.
+
+					<ol>
+						<li>
+							Wat is een utopia die zich kan voldoen door AI?
+						</li>
+						<li>
+							Wat is een dystopia die zich kan voldoen door AI?
+						</li>
+						<li>
+							Hoe moet je AI controleren?
+						</li>
+						<li>
+							Vanaf wanneer is AI gelijkwaardig aan een mens?
+						</li>
+					</ol>
+				</li>
 			</ol>
+
 		</div>
 
 		<div class="bar-s">
@@ -99,35 +125,57 @@ include('../../../components/headerChapter.php');
 		</div>
 
 		<div class="theorie-content theorie-answers">
-			<ol>
+			<ol class="MLquestionAlt2">
 				<li>
-					In een wereld waarin de mensheid succesvol AI leert te controleren zal AI wetenschappelijke velden zoals natuurkunde en wiskunde ondersteunen of zelfs overnemen, hierdoor zal de kwaliteit van menselijk leven omhoog gaan.
+					<ol>
+						<li>
+							AI is hiervoor overbodig en dus niet erg geschikt.
+						</li>
+						<li>
+							Dit is een voorspelling die niet vaak plaats zal vinden en het maken van AI is waarschijnlijk niet de moeite waard, dit kan beter met een 'handmatige' voorspelling worden gedaan.
+						</li>
+						<li>
+							Een AI is hiervoor geschikt, het gaat namelijk om een voorspelling die vaak gedaan moet worden. Het zou heel veel tijd kosten voor een mens om elke foto na te gaan.
+						</li>
+						<li>
+							Een AI is hiervoor geschikt, want het gaat om lastige voorspellingen die vaak gedaan moeten worden. Een dief zal zich namelijk (kunnen) blijven voorplaatsen en er zijn best veel dieven. De AI is dus nuttig en kan best moeite waard zijn.
+						</li>
+					</ol>
 				</li>
 				<li>
-					In een wereld waarin de mensheid AI heeft gemaakt zonder het te kunnen controleren zal de AI de mensheid kunnen manipuleren tot het punt waarbij we allemaal moeten buigen aan een AI overlord. Niet een onbekend probleem.
+					<ol>
+						<li>
+							In een wereld waarin de mensheid succesvol AI leert te controleren zal AI wetenschappelijke velden zoals natuurkunde en wiskunde ondersteunen of zelfs overnemen, hierdoor zal de kwaliteit van menselijk leven omhoog gaan.
+						</li>
+						<li>
+							In een wereld waarin de mensheid AI heeft gemaakt zonder het te kunnen controleren zal de AI de mensheid kunnen manipuleren tot het punt waarbij we allemaal moeten buigen aan een AI overlord. Niet een onbekend probleem.
+						</li>
+						<li>
+							Dus, de sleutel tot een AI utopia is het controleren van de AI. Hoe kunnen we dit doen? Volgens de populaire science fiction schrijver “Isaac Asimov” kan je door middel van drie wetten een AI controleren.
+
+							Een robot mag een mens geen letsel toebrengen, noch, door passief te blijven, een mens letsel laten overkomen.
+
+							Een robot moet de door mensen gegeven orders gehoorzamen behalve wanneer die orders in strijd zijn met de Eerste Wet.
+
+							Een robot moet zichzelf beschermen zolang of voor zover dat niet met de Eerste of Tweede Wet in strijd is.
+
+							Hoewel deze wetten de grootste problemen oplossen, heeft het toch loopholes. Zo kan een AI een de politiek in gaan, het kan nog steeds mensen besturen zonder dat de drie wetten worden verbroken.
+						</li>
+						<li>
+							In het geval van strong AI kan een robot zelfbewustzijn claimen, maar waar kunnen we de lijn van zelfbewustzijn neerzetten?
+
+							In het gedachte-experiment van Frank Jackson wordt de volgende situatie beschreven.
+
+							Mary is meisje dat is opgegroeid in een zwart en witte kamer. Er is een TV in zwart-wit. Mary kan zichzelf niet zien. Ze besteedt haar hele leven om kleuren te bestuderen, wat is blauw, wat is rood etc. Ze ontdekt de golflengtes dat de menselijke oog kan innemen en hoe de brein dit opneemt. Op haar 18e verjaardag krijgt ze als cadeau een rode appel, leert Mary nu iets nieuws?
+
+							De vraag achter dit gedachte-experiment is of er een verschil is tussen het leren van iets en het ervaren van iets. Dit is ook appliceerbaar op het idee van bewustzijn. Als een strong AI (we noemen hem Karel) alles leert over het brein en het brein kan simuleren, heeft Karel dan bewustzijn bereikt? En zo ja, is hij dan gelijkwaardig aan een mens?
+						</li>
+
+					</ol>
 				</li>
-				<li>
-					Dus, de sleutel tot een AI utopia is het controleren van de AI. Hoe kunnen we dit doen? Volgens de populaire science fiction schrijver “Isaac Asimov” kan je door middel van drie wetten een AI controleren.
-
-					Een robot mag een mens geen letsel toebrengen, noch, door passief te blijven, een mens letsel laten overkomen.
-
-					Een robot moet de door mensen gegeven orders gehoorzamen behalve wanneer die orders in strijd zijn met de Eerste Wet.
-
-					Een robot moet zichzelf beschermen zolang of voor zover dat niet met de Eerste of Tweede Wet in strijd is.
-
-					Hoewel deze wetten de grootste problemen oplossen, heeft het toch loopholes. Zo kan een AI een de politiek in gaan, het kan nog steeds mensen besturen zonder dat de drie wetten worden verbroken.
-				</li>
-				<li>
-					In het geval van strong AI kan een robot zelfbewustzijn claimen, maar waar kunnen we de lijn van zelfbewustzijn neerzetten?
-
-					In het gedachte-experiment van Frank Jackson wordt de volgende situatie beschreven.
-
-					Mary is meisje dat is opgegroeid in een zwart en witte kamer. Er is een TV in zwart-wit. Mary kan zichzelf niet zien. Ze besteedt haar hele leven om kleuren te bestuderen, wat is blauw, wat is rood etc. Ze ontdekt de golflengtes dat de menselijke oog kan innemen en hoe de brein dit opneemt. Op haar 18e verjaardag krijgt ze als cadeau een rode appel, leert Mary nu iets nieuws?
-
-					De vraag achter dit gedachte-experiment is of er een verschil is tussen het leren van iets en het ervaren van iets. Dit is ook appliceerbaar op het idee van bewustzijn. Als een strong AI (we noemen hem Karel) alles leert over het brein en het brein kan simuleren, heeft Karel dan bewustzijn bereikt? En zo ja, is hij dan gelijkwaardig aan een mens?
-				</li>
-
 			</ol>
+
+
 		</div>
 	</div>
 

6c01d58dfa8f9a19094308d88a8f8f7f4879db6e
René Steeman <steeman.rene1@gmail.com>
2019-01-23 19:52:29 +0100

    part 4

--- bestanden/newMain.php
+++ bestanden/newMain.php
@@ -25,7 +25,7 @@
 	</div>
 
 	<div class="info">
-		Inforca is een informatica lesmethode gericht op havo en vwo.
+		Inforca is een informatica lesmethode gericht op havo en vwo. Quisque facilisis tempor leo et aliquam. Aliquam nisi massa, pulvinar rutrum ultricies sit amet, eleifend in erat. In facilisis mollis enim vitae lacinia. Proin viverra posuere massa vitae suscipit. Nulla sollicitudin consectetur suscipit. Praesent eget felis quis ex ultrices luctus. Vivamus non consectetur lorem. Curabitur cursus enim quis justo tempus rutrum. Phasellus at nulla velit. Quisque a lorem sit amet nibh ultrices tincidunt in sit amet nisi. Vivamus vitae quam massa. Sed fringilla mi eu porta pellentesque. Proin laoreet rutrum erat, non maximus lorem. Cras ut quam ut ex ullamcorper lacinia eu laoreet diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam euismod metus a elit sagittis consectetur.
 	</div>
 
 	<div class="row">
@@ -62,8 +62,35 @@
 		</h3>
 	</div>
 
-	<div class="row">
+	<div class="info">
+		Inforca is een informatica lesmethode gericht op havo en vwo. Quisque facilisis tempor leo et aliquam. Aliquam nisi massa, pulvinar rutrum ultricies sit amet, eleifend in erat. In facilisis mollis enim vitae lacinia. Proin viverra posuere massa vitae suscipit. Nulla sollicitudin consectetur suscipit. Praesent eget felis quis ex ultrices luctus. Vivamus non consectetur lorem. Curabitur cursus enim quis justo tempus rutrum. Phasellus at nulla velit. Quisque a lorem sit amet nibh ultrices tincidunt in sit amet nisi. Vivamus vitae quam massa. Sed fringilla mi eu porta pellentesque. Proin laoreet rutrum erat, non maximus lorem. Cras ut quam ut ex ullamcorper lacinia eu laoreet diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam euismod metus a elit sagittis consectetur.
+	</div>
 
+	<div class="row">
+		<div class="box">
+			<div class="box-title">
+				Modern
+			</div>
+			<div class="box-content">
+				Uitleg
+			</div>
+		</div>
+		<div class="box">
+			<div class="box-title">
+				Modern
+			</div>
+			<div class="box-content">
+				Uitleg
+			</div>
+		</div>
+		<div class="box">
+			<div class="box-title">
+				Modern
+			</div>
+			<div class="box-content">
+				Uitleg
+			</div>
+		</div>
 	</div>
 
 	<div class="bar">
@@ -72,8 +99,35 @@
 		</h3>
 	</div>
 
-	<div class="row">
+	<div class="info">
+		Inforca is een informatica lesmethode gericht op havo en vwo. Quisque facilisis tempor leo et aliquam. Aliquam nisi massa, pulvinar rutrum ultricies sit amet, eleifend in erat. In facilisis mollis enim vitae lacinia. Proin viverra posuere massa vitae suscipit. Nulla sollicitudin consectetur suscipit. Praesent eget felis quis ex ultrices luctus. Vivamus non consectetur lorem. Curabitur cursus enim quis justo tempus rutrum. Phasellus at nulla velit. Quisque a lorem sit amet nibh ultrices tincidunt in sit amet nisi. Vivamus vitae quam massa. Sed fringilla mi eu porta pellentesque. Proin laoreet rutrum erat, non maximus lorem. Cras ut quam ut ex ullamcorper lacinia eu laoreet diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam euismod metus a elit sagittis consectetur.
+	</div>
 
+	<div class="row">
+		<div class="box">
+			<div class="box-title">
+				Modern
+			</div>
+			<div class="box-content">
+				Uitleg
+			</div>
+		</div>
+		<div class="box">
+			<div class="box-title">
+				Modern
+			</div>
+			<div class="box-content">
+				Uitleg
+			</div>
+		</div>
+		<div class="box">
+			<div class="box-title">
+				Modern
+			</div>
+			<div class="box-content">
+				Uitleg
+			</div>
+		</div>
 	</div>
 
 	<?php

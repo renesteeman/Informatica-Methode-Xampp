82d422270752e0f89e4636f369c5c0f6391bb37b
René Steeman <steeman.rene1@gmail.com>
2019-01-23 10:47:02 +0100

    newMain part 1

---
+++ bestanden/newMain.php
@@ -0,0 +1,74 @@
+<?php
+	include('components/headerIndex.php');
+?>
+
+<link rel="stylesheet" href="css/sales.min.css">
+
+<head>
+	<meta name="description" content="Inforca is een moderne informatica methode. Op de pagina kunt u meer informatie inwinnen over de voordelen ervan." />
+	<meta name="keywords" content="Informatica, lesmethode, betaalbaar, duidelijk, kopen" />
+	<meta name="author" content="René Steeman" />
+</head>
+
+<body>
+
+	<div class="bar-alt">
+		<h3>
+			Wat is Inforca?
+		</h3>
+	</div>
+
+	<div class="row">
+		<div class="box">
+			Modern
+		</div>
+		<div class="box">
+			Voorbereiding
+		</div>
+		<div class="box">
+			Overzichtelijk
+		</div>
+	</div>
+
+
+	<div class="bar-alt">
+		<h3>
+			Waarom Inforca?
+		</h3>
+	</div>
+
+	<div class="row">
+		<div class="box">
+			Modern
+		</div>
+		<div class="box">
+			Voorbereiding
+		</div>
+		<div class="box">
+			Overzichtelijk
+		</div>
+	</div>
+
+	<div class="bar-alt">
+		<h3>
+			Inforca gebruiken
+		</h3>
+	</div>
+
+	<div class="row">
+		<div class="box">
+			Modern
+		</div>
+		<div class="box">
+			Voorbereiding
+		</div>
+		<div class="box">
+			Overzichtelijk
+		</div>
+	</div>
+
+	<?php
+		include('components/footerIndex.php');
+	?>
+
+</body>

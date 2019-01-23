367818cabbba7d07047f5df673d11bab766a4fd9
René Steeman <steeman.rene1@gmail.com>
2019-01-23 21:20:18 +0100

    part 5

--- bestanden/newMain.php
+++ bestanden/newMain.php
@@ -25,36 +25,17 @@
 	</div>
 
 	<div class="info">
-		Inforca is een informatica lesmethode gericht op havo en vwo. Quisque facilisis tempor leo et aliquam. Aliquam nisi massa, pulvinar rutrum ultricies sit amet, eleifend in erat. In facilisis mollis enim vitae lacinia. Proin viverra posuere massa vitae suscipit. Nulla sollicitudin consectetur suscipit. Praesent eget felis quis ex ultrices luctus. Vivamus non consectetur lorem. Curabitur cursus enim quis justo tempus rutrum. Phasellus at nulla velit. Quisque a lorem sit amet nibh ultrices tincidunt in sit amet nisi. Vivamus vitae quam massa. Sed fringilla mi eu porta pellentesque. Proin laoreet rutrum erat, non maximus lorem. Cras ut quam ut ex ullamcorper lacinia eu laoreet diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam euismod metus a elit sagittis consectetur.
-	</div>
+		<p>
+			Inforca is een moderne informatica lesmethode die volledig digitaal beschikbaar is. Het bevat 13 hoofdstukken die ingedeeld zijn in kern- en verdiepingshoofdstukken. Leerlingen kunnen zich hiermee ontwikkelen in verschillende gebieden, zo komen onderwerpen zoals: logica, programmeren, arduino, web development, projecten en databases aan bod.
+		</p>
+		<p>
+			Het heeft naast de theorie ook oefeningen met uitwerkingen waarmee de student zich goed kan voorbereiden op examens en een eventuele toekomst in de ICT. Voor de kernhoofdstukken zijn zelfs oefentoetsen beschikbaar met automatische beoordeling.
+		</p>
+		<p>
+			Voor de docenten zijn ook veel handige systemen beschikbaar, zo kan een docent het overzicht over de leerlingen bewaren. De systemen zorgen er onder anderen voor dat de docent kan zien wie zijn werk af heeft, wie aan het werken is, hoe goed de oefentoetsen gemaakt worden en wie er samenwerken voor projecten.
+		</p>
 
-	<div class="row">
-		<div class="box">
-			<div class="box-title">
-				Modern
-			</div>
-			<div class="box-content">
-				Uitleg
-			</div>
-		</div>
-		<div class="box">
-			<div class="box-title">
-				Modern
-			</div>
-			<div class="box-content">
-				Uitleg
-			</div>
-		</div>
-		<div class="box">
-			<div class="box-title">
-				Modern
-			</div>
-			<div class="box-content">
-				Uitleg
-			</div>
-		</div>
 	</div>
-
 
 	<div class="bar">
 		<h3>
@@ -63,7 +44,15 @@
 	</div>
 
 	<div class="info">
-		Inforca is een informatica lesmethode gericht op havo en vwo. Quisque facilisis tempor leo et aliquam. Aliquam nisi massa, pulvinar rutrum ultricies sit amet, eleifend in erat. In facilisis mollis enim vitae lacinia. Proin viverra posuere massa vitae suscipit. Nulla sollicitudin consectetur suscipit. Praesent eget felis quis ex ultrices luctus. Vivamus non consectetur lorem. Curabitur cursus enim quis justo tempus rutrum. Phasellus at nulla velit. Quisque a lorem sit amet nibh ultrices tincidunt in sit amet nisi. Vivamus vitae quam massa. Sed fringilla mi eu porta pellentesque. Proin laoreet rutrum erat, non maximus lorem. Cras ut quam ut ex ullamcorper lacinia eu laoreet diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam euismod metus a elit sagittis consectetur.
+		<p>
+			Inforca zorgt ervoor dat leerlingen zo veel mogelijk kunnen leren en docenten de leerlingen zo goed mogelijk kan ondersteunen. Dit wordt bereikt door praktische theorie in combinatie met ruimte voor zelfstandige projecten en ondersteunende systemen voor de docenten.
+		</p>
+		<p>
+			Na het doorlopen van alle stof heeft de leerling een goede theoretische en praktische basis in verschillende velden op het gebied van ICT en heeft de leerling ervaring met het uitvoeren van projecten.
+		</p>
+		<p>
+			De docent kan door allerlei handige hulpmiddelen zich meer en efficiënter bezighouden met de leerlingen in plaats van de administratie. Een voorbeeld hiervan is het systeem om projectgroepen te kunnen volgen. Dankzij dit systeem kan in een enkele oogopslag gezien worden wie samenwerken, waaraan ze werken, wat de rollen zijn en waar de bestanden staan.
+		</p>
 	</div>
 
 	<div class="row">
@@ -72,23 +61,50 @@
 				Modern
 			</div>
 			<div class="box-content">
-				Uitleg
+				Alle theorie is up-to-date en eenvoudig bereikbaar via een modern vormgegeven systeem met vele handige functies.
 			</div>
 		</div>
 		<div class="box">
 			<div class="box-title">
-				Modern
+				Uitgebreid
 			</div>
 			<div class="box-content">
-				Uitleg
+				Een wordt een groot aantal onderwerpen uitgebreid besproken en leerlingen krijgen de middelen om hun kennis uitgebreid te toetsen.
 			</div>
 		</div>
 		<div class="box">
 			<div class="box-title">
-				Modern
+				Overzichtelijk
 			</div>
 			<div class="box-content">
-				Uitleg
+				De docent kan eenvoudig het overzicht op de leerlingen bewaren door handige systemen die inbegrepen zijn voor docentenaccounts.
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="box">
+			<div class="box-title">
+				Praktisch
+			</div>
+			<div class="box-content">
+				Leerlingen leren niet alleen pure theorie, maar ook hoe ze de theorie in praktijk kunnen brengen. Het effect hiervan is dat leerlingen beter zijn voorbereid op vervolgopleidingen en informatica leuker gaan vinden.
+			</div>
+		</div>
+		<div class="box">
+			<div class="box-title">
+				Betaalbaar
+			</div>
+			<div class="box-content">
+				Inforca is een stuk goedkoper dan de concurenten, zo kost een account voor een leerling maar €10/jaar en voor een docent maar €30/jaar.
+			</div>
+		</div>
+		<div class="box">
+			<div class="box-title">
+				Privacy
+			</div>
+			<div class="box-content">
+				Inforca waardeert de privacy van leerlingen en docenten. Er wordt daarom alleen gevraagd voor informatie die nodig is voor de werking van de website en administratie. De informatie wordt nooit gedeeld met derden en wordt bijveiligd opgeslagen.
 			</div>
 		</div>
 	</div>

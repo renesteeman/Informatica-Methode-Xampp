8daa87e4569e9e77b4f88ce391ca2aa088e76596
René Steeman <steeman.rene1@gmail.com>
2019-03-08 15:01:44 +0100

    H2.5 and improved css for the header

--- bestanden/pages/theorie/H2/p5.php
+++ bestanden/pages/theorie/H2/p5.php
@@ -54,7 +54,22 @@
 		</div>
 
 		<div class="theorie-content">
-
+			<p>
+				Eindige automaten zijn systemen waarmee mogelijke processen worden vastgelegd. Het klinkt erg lastig, maar weest gerust.
+			</p>
+			<p>
+				Een voorbeeld zal hierbij helpen. Je wilt online een boek gaan kopen, maar weet nog niet welk boek. Je kunt dan een paar stappen volgen en er zijn verschillende uitkomsten mogelijk.
+			</p>
+			<img src="./afbeeldingen/automaat.svg" />
+			<p>
+				Je legt dus vast welke toestanden er mogelijk zijn en hoe toestanden elkaar opvolgen.
+			</p>
+			<p>
+				Als de kans dat een bepaalde route gevolgd wordt 100% is dan is het deterministisch, dat is bij het voorbeeld niet het geval. Het is mogelijk dat je boek wel of niet te duur vindt, het is van tevoren niet zeker.
+			</p>
+			<p>
+				Herhaling is ook mogelijk, zo als te zien is in het voorbeeld bij de pijl van "Je vindt het te duur" naar "Je gaat zoeken".
+			</p>
 
 		</div>
 
@@ -66,7 +81,11 @@
 
 		<div class="theorie-content">
 
-
+			<ol>
+				<li>
+					Maak zelf een automaat voor het gebruiken van een wasstraat. Je mag zelf bepalen wat de mogelijkheden precies zijn.
+				</li>
+			</ol>
 
 		</div>
 
@@ -78,7 +97,11 @@
 
 		<div class="theorie-content theorie-answers">
 
-
+			<ol>
+				<li>
+					<img src="./afbeeldingen/automaatAntwoord.svg" />
+				</li>
+			</ol>
 
 		</div>
 	</div>

2ec9ef024e68089055ae83222a75a35f0e30a245
René Steeman <steeman.rene1@gmail.com>
2019-04-13 07:52:43 +0200

    load chapters part 3

--- bestanden/scripts/loadChapters.php
+++ bestanden/scripts/loadChapters.php
@@ -1,10 +1,10 @@
 <?php
 	include('DB_connect.php');
+	session_save_path('../tmp');
 	session_start();
 
 	$id = $_SESSION["id"];
-  //TODO CHANGE TO ACTUAL SCHOOL
-	$school = 'Inforca';
+	$school = '';
   $error = 0;
   $msg = "";
 
@@ -16,34 +16,54 @@
 		return $data;
 	}
 
-	$sql = "SELECT DISTINCT hoofdstuk, hoofdstuk_naam FROM theorie WHERE school='$school'";
+	$sql = "SELECT school, functie FROM users WHERE id='$id'";
 
 	if (mysqli_query($conn, $sql)) {
+		//find school of teacher
 		$result = mysqli_query($conn, $sql);
+		$result = mysqli_fetch_assoc($result);
+		$school = $result['school'];
+		$functie = $result['functie'];
 
-    if (mysqli_num_rows($result) > 0) {
-      while($row = mysqli_fetch_assoc($result)) {
-        $hoofdstuk = $row["hoofdstuk"];
-        $hoofdstukNaam = $row["hoofdstuk_naam"];
+		if($functie=='docent'){
+			$sql = "SELECT DISTINCT hoofdstuk, hoofdstuk_naam FROM theorie WHERE school='$school' OR school='Inforca'";
 
-        $msg .= '
-          <option value="'.$hoofdstuk.'">'.$hoofdstukNaam.'</option>
-        ';
+			if (mysqli_query($conn, $sql)) {
+				$result = mysqli_query($conn, $sql);
 
-      }
-    } else {
-      $msg .= '
-        <option value=""></option>
-      ';
-    }
+		    if (mysqli_num_rows($result) > 0) {
+					$msg.='yee';
+		      while($row = mysqli_fetch_assoc($result)) {
+		        $hoofdstuk = $row["hoofdstuk"];
+		        $hoofdstukNaam = $row["hoofdstuk_naam"];
 
-    $msg .= '
-      <option value="Aanmaken">Aanmaken</option>
-    ';
+		        $msg .= '
+		          <option value="'.$hoofdstuk.'">'.$hoofdstukNaam.'</option>
+		        ';
+
+		      }
+		    } else {
+		      $msg .= '
+		        <option value=""></option>
+		      ';
+		    }
+
+		    $msg .= '
+		      <option value="Aanmaken">Aanmaken</option>
+		    ';
+
+			} else {
+				$msg .= "\nEr is een fout opgetreden met SQL, neem alstublieft contact op met info@inforca.nl en noem zowel de pagina als de inhoud van dit bericht. Alvast erg bedankt!";
+		    $error = 1;
+			}
+		} else {
+			$msg.="\nU bent geen docent.";
+			$error = 1;
+		}
 
 	} else {
 		$msg .= "\nEr is een fout opgetreden met SQL, neem alstublieft contact op met info@inforca.nl en noem zowel de pagina als de inhoud van dit bericht. Alvast erg bedankt!";
-    $error = 1;
+		$error = 1;
 	}
 
   $toReturn = array('msg' => $msg, 'error' => $error);

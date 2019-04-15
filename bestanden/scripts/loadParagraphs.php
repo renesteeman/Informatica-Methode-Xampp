50279a7ac703f8bdac410a14faf23f0e117dee43
René Steeman <steeman.rene1@gmail.com>
2019-04-15 17:38:18 +0200

    load paragraphs now uses the school's paragraph if it exists and the default one otherwise

--- bestanden/scripts/loadParagraphs.php
+++ bestanden/scripts/loadParagraphs.php
@@ -8,6 +8,15 @@
   $functie = "";
   $error = 0;
   $msg = "";
+	$chapter = "";
+
+	$school_theory_ids = [];
+	$school_theory_paragraphs = [];
+	$inforca_theory_ids = [];
+	$inforca_theory_paragraphs = [];
+
+	$theory_ids = [];
+	$theory_paragraphs = [];
 
 	//function to check and clean input
 	function check_input($data) {
@@ -17,7 +26,8 @@
 		return $data;
 	}
 
-  $chapter = mysqli_real_escape_string($conn, check_input($_POST['chapter']));
+  //TODO $chapter = mysqli_real_escape_string($conn, check_input($_POST['chapter']));
+	$chapter = "H1";
 
 	$sql = "SELECT school, functie FROM users WHERE id='$id'";
 
@@ -29,7 +39,7 @@
 		$functie = $result['functie'];
 
 		if($functie=='docent'){
-			$sql = "SELECT DISTINCT theory_id, paragraaf_naam FROM theorie WHERE (school='$school' OR school='Inforca') AND hoofdstuk='$chapter'";
+			$sql = "SELECT theory_id, paragraaf FROM theorie WHERE school='$school' AND hoofdstuk='$chapter'";
 
 			if (mysqli_query($conn, $sql)) {
 				$result = mysqli_query($conn, $sql);
@@ -37,22 +47,64 @@
 		    if (mysqli_num_rows($result) > 0) {
 		      while($row = mysqli_fetch_assoc($result)) {
 		        $theory_id = $row["theory_id"];
-		        $paragraaf_naam = $row["paragraaf_naam"];
+		        $paragraaf = $row["paragraaf"];
 
-		        $msg .= '
-		          <option value="'.$theory_id.'">'.$paragraaf_naam.'</option>
-		        ';
-
+						$school_theory_ids[] = $theory_id;
+						$school_theory_paragraphs[] = $paragraaf;
 		      }
-		    } else {
-		      $msg .= '
-		        <option value=""></option>
-		      ';
-		    }
+				}
 
-		    $msg .= '
-		      <option value="Aanmaken">Aanmaken</option>
-		    ';
+				$sql = "SELECT theory_id, paragraaf FROM theorie WHERE school='Inforca' AND hoofdstuk='$chapter'";
+
+				if (mysqli_query($conn, $sql)) {
+					$result = mysqli_query($conn, $sql);
+
+					if (mysqli_num_rows($result) > 0) {
+						while($row = mysqli_fetch_assoc($result)) {
+							$theory_id = $row["theory_id"];
+							$paragraaf = $row["paragraaf"];
+
+							$inforca_theory_ids[] = $theory_id;
+							$inforca_theory_paragraphs[] = $paragraaf;
+						}
+					}
+
+					$theory_ids = $inforca_theory_ids;
+					$theory_paragraphs = $inforca_theory_paragraphs;
+
+					for($i=0; $i<count($school_theory_paragraphs);$i++){
+						$paragraphSchool = $school_theory_paragraphs[$i];
+						$array_found_index = array_search($paragraphSchool, $inforca_theory_paragraphs);
+
+						//check if a paragrpahs from the school is found and if so replace it by the school version
+						if($array_found_index !== False){
+							$theory_ids[$array_found_index] = $school_theory_ids[$i];
+							$theory_paragraphs[$array_found_index] = $school_theory_paragraphs[$i];
+						}
+					}
+
+					if(count($theory_ids) > 0){
+						for($i=0; $i<count($theory_ids); $i++) {
+
+							$theory_id = $theory_ids[$i];
+							$paragraaf_name = $theory_paragraphs[$i];
+
+							$msg .= '<option value="'.$theory_id.'">'.$paragraaf_name.'</option>';
+						}
+					}
+					//TODO remove when create option is added
+					else {
+						$msg .= '<option value=""></option>';
+					}
+
+					/*
+					$msg .= '<option value="Aanmaken">Aanmaken</option>';
+					*/
+
+				} else {
+					$msg .= "\nEr is een fout opgetreden met SQL, neem alstublieft contact op met info@inforca.nl en noem zowel de pagina als de inhoud van dit bericht. Alvast erg bedankt!";
+					$error = 1;
+				}
 
 			} else {
 				$msg .= "\nEr is een fout opgetreden met SQL, neem alstublieft contact op met info@inforca.nl en noem zowel de pagina als de inhoud van dit bericht. Alvast erg bedankt!";
